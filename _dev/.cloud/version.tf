d1fea701695852932222fcf3bdba1d534f1e45c4
Maxence Perrin <maxence.perrin@prestashop.com>
2021-04-20 11:01:15 +0200

    Fixed terraform version file

--- _dev/.cloud/version.tf
+++ _dev/.cloud/version.tf
@@ -1,35 +1,13 @@
-locals {
-  project = local.config.project[terraform.workspace]
-  managed_zone = local.config.managed_zone[terraform.workspace]
-  environment = local.config.environment[terraform.workspace]
-  url = local.config.url[terraform.workspace]
-
-  config = {
-    project = {
-      alpha = "psessentials-integration"
-      stable = "psessentials-production"
+terraform {
+  required_providers {
+    google = {
+      version = "3.40.0"
+      source = "hashicorp/google"
     }
-    managed_zone = {
-      alpha   = "psessentials-integration-net"
-      stable  = "psessentials-net"
-    }
-    environment = {
-      alpha = "integration"
-      stable = "production"
-    }
-    url = {
-      alpha = "storybook-google.psessentials-integration.net"
-      stable = "storybook-google.psessentials.net"
+    kubernetes = {
+      version = "1.10.0"
+      source = "hashicorp/kubernetes"
     }
   }
-}
-
-variable "app_version" {
-  description = "UI app tag version"
-  default = "latest"
-}
-
-variable "hash_id" {
-  description = "Github hash"
-  default     = "latest"
+  required_version = "0.14.5"
 }
