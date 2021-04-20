2ff5dc1efdab01519fef16990d09978a3184cbd2
Amaury Hanser <amaury.hanser@gmail.com>
2021-04-20 17:15:55 +0200

    Adds Stepper component

---
+++ _dev/src/components/commons/stepper.vue
@@ -0,0 +1,59 @@
+<template>
+  <ol class="ps_gs-stepper position-relative justify-content-between p-0 mb-3">
+    <li
+      v-for="(step, index) in steps"
+      :key="step.title"
+      class="ps_gs-stepper-step col position-relative"
+      :class="{
+        'active': step.active,
+        'complete': isComplete(index)
+      }"
+    >
+      <div class="ps_gs-stepper-step__step d-flex align-items-center justify-content-center">
+        <b-icon-check
+          v-if="isComplete(index)"
+          variant="white"
+          font-scale="2"
+        />
+        <span
+          v-else
+        >
+          {{ index + 1}}
+        </span>
+      </div>
+      <div
+        :class="{'text-muted': !step.active && !isComplete(index)}"
+        class="ps_gs-stepper-step__title ps_gs-fz-12 text-center"
+      >
+        {{ step.title }}
+      </div>
+    </li>
+  </ol>
+</template>
+
+<script>
+import {
+  BIconCheck,
+} from 'bootstrap-vue';
+
+
+export default {
+  name: 'stepper',
+  components: { BIconCheck },
+  data() {
+    return {
+    };
+  },
+  methods: {
+    isComplete(index) {
+      const indexActive = this.steps.map(i => i.active).indexOf(true);
+      return index < indexActive;
+    },
+  },
+  props: {
+    steps: {
+      type: Array,
+    },
+  },
+}
+</script>
---
+++ _dev/stories/stepper.stories.ts
@@ -0,0 +1,38 @@
+import Stepper from '../src/components/commons/stepper.vue'
+
+export default {
+  title: 'Base/Horizontal Stepper',
+  component: Stepper,
+};
+
+const Template = (args, { argTypes }) => ({
+  props: Object.keys(argTypes),
+  components: { Stepper },
+  template: '<Stepper v-bind="$props" />',
+});
+
+export const HorizontalStepper:any = Template.bind({});
+HorizontalStepper.args = {
+  steps: [
+    {
+      title: "Shipping settings",
+      active: true,
+    },
+    {
+      title: "Export rules",
+      active: false,
+    },
+    {
+      title: "Attribute mapping",
+      active: false,
+    },
+    {
+      title: "Category mapping",
+      active: false,
+    },
+    {
+      title: "Summary",
+      active: false,
+    },
+  ],
+}
