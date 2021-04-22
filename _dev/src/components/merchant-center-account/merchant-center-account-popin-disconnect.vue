9b623d950c422179ed398079bccc4f65c5b316fd
Amaury Hanser <amaury.hanser@gmail.com>
2021-04-22 09:25:46 +0200

    Popin MCA disconnect / init

---
+++ _dev/src/components/merchant-center-account/merchant-center-account-popin-disconnect.vue
@@ -0,0 +1,28 @@
+<template>
+  <b-modal
+    :visible="true"
+    id="MerchantCenterAccountPopinDisconnect"
+    title="Confirm disconnection?"
+    title-class="font-weight-600"
+  >
+    <p class="my-4">You are about to disconnect Google Merchant Center account. This will remove access to Google Merchant Center and Google Ads accounts.</p>
+    <template slot="modal-ok">
+      Disconnect
+    </template>
+  </b-modal>
+</template>
+
+<script>
+import { BModal } from 'bootstrap-vue';
+
+export default {
+  name: "MerchantCenterAccountPopinDisconnect",
+  components: {
+    BModal,
+  },
+  data() {
+    return {
+    };
+  },
+};
+</script>
---
+++ _dev/stories/merchant-center-card-popin-disconnect.stories.ts
@@ -0,0 +1,21 @@
+import MerchantCenterAccountPopinDisconnect from '../src/components/merchant-center-account/merchant-center-account-popin-disconnect.vue'
+import OnboardingPage from '../src/views/onboarding-page.vue'
+
+export default {
+  title: 'Merchant Center Account/Popin Disconnect',
+  component: MerchantCenterAccountPopinDisconnect, OnboardingPage
+};
+
+const Template = (args, { argTypes }) => ({
+  props: Object.keys(argTypes),
+  components: { MerchantCenterAccountPopinDisconnect, OnboardingPage},
+  template: `
+    <div>
+      <OnboardingPage />
+      <MerchantCenterAccountPopinDisconnect />
+    </div>
+  `,
+});
+
+export const PopinDisconnect:any = Template.bind({});
+PopinDisconnect.args = {}
