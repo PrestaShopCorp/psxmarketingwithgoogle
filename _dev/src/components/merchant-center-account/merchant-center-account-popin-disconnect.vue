159c130219aabc715737c9d80d3b9c65fc7d5e6a
Amaury Hanser <amaury.hanser@gmail.com>
2021-04-23 14:15:19 +0200

    Adds ps-modal component
    ps-modal component extends b-modal from bootstrap-vue

--- _dev/src/components/merchant-center-account/merchant-center-account-popin-disconnect.vue
+++ _dev/src/components/merchant-center-account/merchant-center-account-popin-disconnect.vue
@@ -1,29 +1,25 @@
 <template>
-  <b-modal
+  <ps-modal
     :visible="true"
     id="MerchantCenterAccountPopinDisconnect"
     title="Confirm disconnection?"
-    modal-class="ps_gs-modal"
-    title-class="ps_gs-modal__title"
-    header-class="ps_gs-modal__header"
-    content-class="ps_gs-modal__content"
-    footer-class="ps_gs-modal__footer"
-    cancel-variant="outline-secondary"
   >
-    <p class="my-1">You are about to disconnect Google Merchant Center account. This will remove access to Google Merchant Center and Google Ads accounts.</p>
+    <p class="my-1">
+      You are about to disconnect Google Merchant Center account. This will remove access to Google Merchant Center and Google Ads accounts.
+    </p>
     <template slot="modal-ok">
       Disconnect account
     </template>
-  </b-modal>
+  </ps-modal>
 </template>
 
 <script>
-import { BModal } from 'bootstrap-vue';
+import PsModal from '../commons/ps-modal'
 
 export default {
   name: "MerchantCenterAccountPopinDisconnect",
   components: {
-    BModal,
+    PsModal
   },
   data() {
     return {
