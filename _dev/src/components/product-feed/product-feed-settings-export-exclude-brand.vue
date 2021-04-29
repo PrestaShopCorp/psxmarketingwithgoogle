35588aea82f007857317865945a5170d92cb4a06
Amaury Hanser <amaury.hanser@gmail.com>
2021-04-29 17:18:16 +0200

    Adds exclude by brand / category

---
+++ _dev/src/components/product-feed/product-feed-settings-export-exclude-brand.vue
@@ -0,0 +1,125 @@
+<template>
+  <b-form-group
+    class="mt-0 pb-2"
+  >
+  <label class="ps_gs-fz-14 font-weight-600 mb-2 p-0 d-block">
+    Select brand
+  </label>
+    <ps-select
+      :reduce="options => options.name"
+      :options="options.filter(o => selectedBrand.indexOf(o.name) < 0)"
+      :deselect-from-dropdown="true"
+      multiple
+      @input="pushSelectedBrands"
+      @search="searchProducts"
+      label="name"
+      :placeholder="$t('productFeedSettings.export.searchProducts')"
+      class="maxw-sm-500"
+      :class="{ 'has-selection': selectedBrand.length > 0 }"
+    >
+      <template v-slot:option="option">
+        <span v-html="highlightSearch(`${option.name}`)" />
+      </template>
+    </ps-select>
+  </b-form-group>
+</template>
+
+<script>
+import PsSelect from '../commons/ps-select';
+
+export default {
+  name: 'ProductFeedSettingsExportExcludeBrand',
+  data() {
+    return {
+      selectedBrand: [],
+      searchString: '',
+      options: [
+        {
+            id: '1',
+            name: 'Adidas',
+        },
+        {
+            id: '2',
+            name: 'Nike',
+        },
+        {
+            id: '3',
+            name: 'Hummel',
+        },
+        {
+            id: '4',
+            name: 'Kappa',
+        },
+        {
+            id: '5',
+            name: 'Le Coq Sportif',
+        },
+        {
+            id: '6',
+            name: 'New Balance',
+        },
+        {
+            id: '7',
+            name: 'Celio',
+        },
+        {
+            id: '8',
+            name: 'Suitsupply',
+        },
+        {
+            id: '9',
+            name: 'Burberry',
+        },
+        {
+            id: '10',
+            name: 'Bruce field',
+        },
+        {
+            id: '11',
+            name: 'Cos',
+        },
+        {
+            id: '12',
+            name: 'Jules',
+        },
+        {
+            id: '13',
+            name: 'Devred',
+        },
+        {
+            id: '14',
+            name: 'Pied de biche',
+        },
+        {
+            id: '15',
+            name: 'Bexley',
+        },
+        {
+            id: '16',
+            name: 'Charles tyrwhitt',
+        },
+      ],
+    };
+  },
+  components: {
+    PsSelect,
+  },
+  methods: {
+    pushSelectedBrands(event) {
+      this.selectedBrand = event;
+    },
+    searchProducts(event) {
+      this.searchString = event;
+    },
+    highlightSearch(str) {
+      /** Highlight search terms */
+      const regex = new RegExp(`(${this.searchString})`, 'gi');
+      return str.replace(regex, '<strong>$1</strong>');
+    },
+  },
+  computed: {
+  },
+  mounted() {
+  },
+};
+</script>
---
+++ _dev/src/components/product-feed/product-feed-settings-export-exclude-category.vue
@@ -0,0 +1,72 @@
+<template>
+  <b-form-group
+    class="mt-4 pb-2"
+  >
+    <p class="font-weight-600 mb-1 p-0 d-block">
+      Select categories to export
+    </p>
+    <ul class="ps_gs-categories-list">
+      <li>
+        <b-form-checkbox
+          class="ps_gs-checkbox"
+          name="allCategories"
+          :indeterminate.sync="allCategoriesIndeterminate"
+          inline
+        >
+          All categories
+        </b-form-checkbox>
+        <ul>
+          <li v-for="(n,index) in 5" :key="index">
+            <b-form-checkbox
+              class="ps_gs-checkbox"
+              name="allCategories"
+              inline
+            >
+              Catégorie {{ n }}
+            </b-form-checkbox>
+            <ul>
+              <li v-for="(n,index) in 3" :key="index">
+                <b-form-checkbox
+                  class="ps_gs-checkbox"
+                  name="allCategories"
+                  inline
+                >
+                  Catégorie {{ n }}
+                </b-form-checkbox>
+                <ul>
+                  <li v-for="(n,index) in 2" :key="index">
+                    <b-form-checkbox
+                      class="ps_gs-checkbox"
+                      name="allCategories"
+                      inline
+                    >
+                      Catégorie {{ n }}
+                    </b-form-checkbox>
+                  </li>
+                </ul>
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </li>
+    </ul>
+  </b-form-group>
+</template>
+
+<script>
+
+export default {
+  name: 'ProductFeedSettingsExportExcludeCategory',
+  data() {
+    return {
+      allCategoriesIndeterminate: true,
+    };
+  },
+  methods: {
+  },
+  computed: {
+  },
+  mounted() {
+  },
+};
+</script>
