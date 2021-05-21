2092d43f55db66166b34efa6a454a91b282e6050
Amaury Hanser <amaury.hanser@gmail.com>
2021-05-21 09:05:10 +0200

    Adds ps-toast component

---
+++ _dev/src/components/commons/ps-toast.vue
@@ -0,0 +1,21 @@
+<template>
+	<b-alert
+    class="ps_gs-toast pr-5"
+    v-bind="$attrs"
+    v-on="$listeners"
+    dismissible
+    fade
+  >
+		<slot></slot>
+		<slot name="dismiss"></slot>
+	</b-alert>
+</template>
+<script>
+	import { BAlert } from 'bootstrap-vue';
+
+	export default {
+		name: 'PsToast',
+		inheritAttrs: false,
+		components: { BAlert },
+	}
+</script>
---
+++ _dev/stories/toast.stories.ts
@@ -0,0 +1,26 @@
+import PsToast from '../src/components/commons/ps-toast.vue'
+
+export default {
+  title: 'Basic Components/Toast',
+  component: PsToast,
+};
+
+const Template = (args, { argTypes }) => ({
+  props: Object.keys(argTypes),
+  components: { PsToast },
+  template: `
+    <div>
+      <ps-toast
+        variant="success"
+        :show="isToastShown"
+      >
+        Fixed position (top) alert!
+      </ps-toast>
+    </div>
+  `,
+});
+
+export const Toast:any = Template.bind({});
+Toast.args = {
+  isToastShown: 1,
+}
