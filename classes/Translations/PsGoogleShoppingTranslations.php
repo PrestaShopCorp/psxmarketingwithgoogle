becb4922770a9853268a256eeb3a70777967ef88
Amaury Hanser <amaury.hanser@gmail.com>
2021-06-11 17:09:12 +0200

    Sync php translation file with json one

--- classes/Translations/PsGoogleShoppingTranslations.php
+++ classes/Translations/PsGoogleShoppingTranslations.php
@@ -69,17 +69,17 @@ class PsGoogleShoppingTranslations
                     'content1' => [
                         'title' => $this->module->l('Connect your store to Google Merchant Center', 'PsGoogleShoppingTranslations'),
                         'text' => $this->module->l('Connecting your store to Google Merchant Center allows you to upload store and product data to Google. Your products will automatically sync to make relevant information available for free listings, Google Ads, and other Google services.', 'PsGoogleShoppingTranslations'),
-                        'footer' => $this->module->l('You can create a new [Merchant Center account](https://support.google.com/merchants/answer/7679093)[:target="_blank"] or link an existing account. Note that you’ll need to meet the Merchant Center [eligibility requirements](https://support.google.com/merchants/answer/9199328)[:target="_blank"] in order to connect your store.', 'PsGoogleShoppingTranslations'),
+                        'footer' => $this->module->l('You can create a new [Merchant Center account]({0})[:target=\"_blank\"] or link an existing account. Note that you’ll need to meet the Merchant Center [eligibility requirements]({1})[:target=\"_blank\"] in order to connect your store.', 'PsGoogleShoppingTranslations'),
                     ],
                     'content2' => [
                         'title' => $this->module->l('Reach online shoppers with free listings', 'PsGoogleShoppingTranslations'),
                         'text' => $this->module->l('Free listings drive traffic by showcasing your products to customers who are actively looking for what you sell. Your products will appear on the Google Shopping tab, and can also appear on Google Search, Google Images, and Gmail depending on regional availability.', 'PsGoogleShoppingTranslations'),
-                        'footer' => $this->module->l('[Learn more about free listings](https://support.google.com/merchants/answer/9826670)[:target="_blank"]', 'PsGoogleShoppingTranslations'),
+                        'footer' => $this->module->l('[Learn more about free listings]({0})[:target=\"_blank\"]', 'PsGoogleShoppingTranslations'),
                     ],
                     'content3' => [
                         'title' => $this->module->l('Boost store traffic and sales with Google Ads', 'PsGoogleShoppingTranslations'),
                         'text' => $this->module->l('Smart Shopping campaigns help you reach even more customers and grow your business by promoting your products across Google Search, Shopping, YouTube, Gmail, and the Display Network. They use Google’s technology to optimize your ads and automatically show your products to customers who are more likely to buy and spend more.', 'PsGoogleShoppingTranslations'),
-                        'footer' => $this->module->l('[Learn more about Smart Shopping campaigns](https://support.google.com/google-ads/answer/7674739?hl=en)[:target="_blank"]', 'PsGoogleShoppingTranslations'),
+                        'footer' => $this->module->l('[Learn more about Smart Shopping campaigns]({0})[:target=\"_blank\"]', 'PsGoogleShoppingTranslations'),
                     ],
                 ],
                 'footer' => [
@@ -111,11 +111,11 @@ class PsGoogleShoppingTranslations
             'mcaCard' => [
                 'title' => $this->module->l('Make your products available to shoppers accross Google', 'PsGoogleShoppingTranslations'),
                 'introDisabled' => $this->module->l('Associate your Merchant Center account to get your store and products information into Google', 'PsGoogleShoppingTranslations'),
-                'introEnabled' => $this->module->l('[Google Merchant Center](//google.com)[:target="_blank"] helps you get your store and product info into Google and make it available to shoppers across Google.', 'PsGoogleShoppingTranslations'),
+                'introEnabled' => $this->module->l('[Google Merchant Center]({0})[:target=\"_blank\"] helps you get your store and product info into Google and make it available to shoppers across Google.', 'PsGoogleShoppingTranslations'),
                 'labelSelect' => $this->module->l('Connect an existing Merchant Center account', 'PsGoogleShoppingTranslations'),
                 'claimCollides' => $this->module->l('Your current website claim collides with an existing claim', 'PsGoogleShoppingTranslations'),
                 'claimOverwrite' => $this->module->l('To finalize your Google Merchant Center account creation, you need to overwrite the existing claim.', 'PsGoogleShoppingTranslations'),
-                'footerEU' => $this->module->l("If you are in the European Economic Area or Switzerland your Merchant Center account must be associated with a [Comparison Shopping Service (CSS)](//google.com){:target=\"_blank\"}.  \n  If you create a new Merchant Center account through this application, it will be associated with Google Shopping, Google’s CSS, by default. You can change the CSS associated with your account at any time. [Find your CSS Partners](//google.com){:target=\"_blank\"}  \n  Once you have set up your Merchant Center account you can use our onboarding tool regardless of which CSS you use.", 'PsGoogleShoppingTranslations'),
+                'footerEU' => $this->module->l("If you are in the European Economic Area or Switzerland your Merchant Center account must be associated with a [Comparison Shopping Service (CSS)]({0})[:target=\"_blank\"]  \nIf you create a new Merchant Center account through this application, it will be associated with Google Shopping, Google’s CSS, by default. You can change the CSS associated with your account at any time. [Find your CSS Partners]({1})[:target=\"_blank\"]  \nOnce you have set up your Merchant Center account you can use our onboarding tool regardless of which CSS you use.", 'PsGoogleShoppingTranslations'),
                 'shopInfoMissing' => $this->module->l('Shop information is missing', 'PsGoogleShoppingTranslations'),
                 'shopInfoMissingDescription' => $this->module->l('To check if it is a validity issue or if you are not following our rules, please log in to your account a', 'PsGoogleShoppingTranslations'),
                 'userIsNotAdmin' => $this->module->l('You need Administrator access', 'PsGoogleShoppingTranslations'),
@@ -172,7 +172,7 @@ class PsGoogleShoppingTranslations
                 'businessPhone' => $this->module->l('Business phone number', 'PsGoogleShoppingTranslations'),
                 'siteContainsAdultContent' => $this->module->l('My site contains adult content', 'PsGoogleShoppingTranslations'),
                 'seePolicyAdultContent' => $this->module->l('See policy on adult-oriented content', 'PsGoogleShoppingTranslations'),
-                'labelReadAndAgree' => $this->module->l('By purchasing Shopping ads, I have read and agree to comply with [Google\'s terms and policies](//google.com)[:target="_blank"], including [Google’s Merchant Center terms of service](//google.com)[:target="_blank"], [Shopping ads policies](//google.com)[:target="_blank"], and [Google Ads Terms and Conditions](//google.com)[:target="_blank"].', 'PsGoogleShoppingTranslations'),
+                'labelReadAndAgree' => $this->module->l('By purchasing Shopping ads, I have read and agree to comply with [Google\'s terms and policies]({0})[:target=\"_blank\"], including [Google’s Merchant Center terms of service]({1})[:target=\"_blank\"], [Shopping ads policies]({2})[:target=\"_blank\"], and [Google Ads Terms and Conditions]({3})[:target=\"_blank\"].', 'PsGoogleShoppingTranslations'),
                 'alert' => $this->module->l('If one of these requirements is missing, your Merchant Center account will be disapproved.', 'PsGoogleShoppingTranslations'),
             ],
             'productFeedCard' => [
