fa527226f15dc3af9617cd0a0271101ecd506ed7
Elise Griset <eGriset@PRESTAMAC-094.local>
2021-06-23 08:45:03 +0200

    add new view multistore

---
+++ _dev/src/views/multistore.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="pt-2">
+    <template v-if="!psAccountsContext.isShopContext && shops.length">
+      <b-alert
+        v-if="!psAccountsContext.isShopContext && shops.length"
+        show
+        variant="warning"
+        class="mb-0 mt-3 mb-3"
+      >
+        {{ $t('onboarding.warningMultistore') }}
+      </b-alert>
+      <MultiStoreSelector
+        v-if="!psAccountsContext.isShopContext && shops.length"
+        :shops="shops"
+        @shop-selected="onShopSelected($event)"
+      />
+    </template>
+    <landing-page v-else />
+  </div>
+</template>
+
+<script>
+import {MultiStoreSelector} from 'prestashop_accounts_vue_components';
+import LandingPage from './landing-page.vue';
+
+export default {
+  name: 'OnboardingPage',
+  components: {
+    MultiStoreSelector,
+    LandingPage,
+  },
+  data() {
+    return {
+      isMcaLinking: false,
+    };
+  },
+  methods: {
+    onShopSelected(shopSelected) {
+      window.location.href = shopSelected.url;
+    },
+
+    onSyncToggled() {
+      // If user has sync already, we warn the user that the sync won't work anymore with modal
+      if (this.$store.state.productFeed.status.enabled) {
+        this.$bvModal.show(
+          this.$refs.productFeedDisableModal.$refs.modal.id,
+        );
+      // Else (user has not sync) we toggle the sync for the user
+      } else {
+        this.$store.dispatch('productFeed/TOGGLE_SYNCHRONIZATION', true);
+      }
+    },
+  },
+  computed: {
+
+    psAccountsContext() {
+      return this.$store.getters['accounts/GET_PS_ACCOUNTS_CONTEXT'];
+    },
+    shops() {
+      return this.$store.getters['accounts/GET_PS_ACCOUNTS_CONTEXT_SHOPS'];
+    },
+  },
+};
+</script>
