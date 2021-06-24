554fab2b5e24b974e75f2bd3ed7741e59a82a0d9
Amaury Hanser <amaury.hanser@gmail.com>
2021-06-24 11:21:11 +0200

    Uses one file for all free-listing alert stories

--- _dev/stories/free-listing-card.stories.ts
+++ _dev/stories/free-listing-card.stories.ts
@@ -1,4 +1,6 @@
 import FreeListingCard from '../src/components/free-listing/free-listing-card.vue'
+import {enableProductFeed, disableProductFeed} from '../.storybook/mock/product-feed';
+import {enableFreeListing, disableFreeListing} from '../.storybook/mock/free-listing';
 
 export default {
   title: 'Free listing/Card',
@@ -9,6 +11,7 @@ const Template = (args, { argTypes }) => ({
   props: Object.keys(argTypes),
   components: { FreeListingCard },
   template: '<FreeListingCard v-bind="$props" />',
+  beforeCreate : args.beforeCreate,
 });
 
 export const Disabled:any = Template.bind({});
@@ -19,4 +22,48 @@ Disabled.args = {
 export const Enabled:any = Template.bind({});
 Enabled.args = {
   isEnabled: true,
+}
+
+export const AlertEnableFreeListing:any = Template.bind({});
+AlertEnableFreeListing.args = {
+  isEnabled: true,
+  firstTime: false,
+  enabledFreeListing: false,
+  beforeCreate(this: any) {
+    this.$store.state.productFeed = enableProductFeed
+    this.$store.state.freeListing = disableFreeListing
+  },
+}
+
+export const AlertActivationSuccess:any = Template.bind({});
+AlertActivationSuccess.args = {
+  isEnabled: true,
+  firstTime: false,
+  enabledFreeListing: true,
+  beforeCreate(this: any) {
+    this.$store.state.productFeed = enableProductFeed
+    this.$store.state.freeListing = enableFreeListing
+  },
+}
+
+export const AlertEnableFreeListingAndProductFeed:any = Template.bind({});
+AlertEnableFreeListingAndProductFeed.args = {
+  isEnabled: true,
+  firstTime: false,
+  enabledFreeListing: false,
+  beforeCreate(this: any) {
+    this.$store.state.productFeed = disableProductFeed;
+    this.$store.state.freeListing = disableFreeListing;
+  },
+}
+
+export const AlertProductFeedDisabled:any = Template.bind({});
+AlertProductFeedDisabled.args = {
+  isEnabled: true,
+  firstTime: false,
+  enabledFreeListing: true,
+  beforeCreate(this: any) {
+    this.$store.state.productFeed = disableProductFeed;
+    this.$store.state.freeListing = enableFreeListing;
+  },
 }
