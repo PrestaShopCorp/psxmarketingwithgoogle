5e48d1e463bf15733ac7fe3d840d86afcdecac6c
Thomas Nabord <thomas.nabord@prestashop.com>
2021-07-14 16:05:03 +0100

    Mock vue i18n

--- _dev/tests/unit/init.js
+++ _dev/tests/unit/init.js
@@ -1,29 +1,43 @@
-import { createLocalVue } from '@vue/test-utils';
+import { config, createLocalVue } from '@vue/test-utils';
 import VueRouter from 'vue-router';
 import Vuex from 'vuex';
 import translations from '../../.storybook/translations.json';
 
 let windowSpy, localVue;
-  
-beforeEach(() => {
-  windowSpy = jest.spyOn(window, "window", "get");
-  
-  windowSpy.mockImplementation(() => {
-    return {
-      i18nSettings: {
-        isoCode: 'en',
-      },
-      translations,
-    };
-  });
+const defaultLocale = 'en';
 
-  localVue = createLocalVue();
-  localVue.use(Vuex);
-  localVue.use(VueRouter);
+beforeEach(() => {
+    windowSpy = jest.spyOn(window, "window", "get");
+    
+    windowSpy.mockImplementation(() => {
+        return {
+            // add data needed in window
+        };
+    });
+    
+    localVue = createLocalVue();
+    localVue.use(Vuex);
+    localVue.use(VueRouter);
 });
 
 afterEach(() => {
   windowSpy.mockRestore();
 });
 
-export default localVue;
+config.mocks.$t = key => {
+    const parts = key.split( "." ),
+        length = parts.length;
+    let property = translations[defaultLocale];
+
+    for (let i = 0; i < length; i++ ) {
+        property = property[parts[i]];
+    }
+
+    return property;
+};
+
+export default {
+    commonOptions: {
+        localVue,
+    }
+};
--- _dev/tests/unit/landing-page.spec.js
+++ _dev/tests/unit/landing-page.spec.js
@@ -3,7 +3,7 @@
  */
 
 // Import this file first to init mock on window
-import { localVue } from './init.js';
+import { commonOptions } from './init.js';
 
 import { shallowMount } from '@vue/test-utils';
 import Vuex from 'vuex';
@@ -13,15 +13,13 @@ import store from '@/store';
 describe('landing-page.vue', () => {
   it('show all the onboarding details', () => {
     const wrapper = shallowMount(LandingPage, {
+      ...commonOptions,
       store: new Vuex.Store(Object.assign({}, store, {
         actions: {
         },
         mutations: {
         },
       })),
-      localVue,
-      // Can't get it with import because of the var window
-      i18n: require('@/lib/i18n'),
     });
 
     expect(wrapper.find('.ps_gs-landingpage').isVisible()).toBe(true);
