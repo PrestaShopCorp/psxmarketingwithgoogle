a7e728930d3b7ecec1dbac1254277cd4f9c261f7
Elise Griset <eGriset@PRESTAMAC-094.local>
2021-07-19 17:49:35 +0200

    lint

--- _dev/src/utils/Filters.ts
+++ _dev/src/utils/Filters.ts
@@ -31,14 +31,14 @@ Vue.filter(
 Vue.filter(
   'changeCountryCodeToName', (countriesGiven : Array<string>, totalCountries : Array<{code: string; country: string}>) => {
     const final : string[] = [];
-      countriesGiven.map((data) => {
-        for (let i = 0; i < totalCountries.length; i += 1) {
-          if (data === totalCountries[i].code) {
-            final.push(totalCountries[i].country);
-            break;
-          }
+    countriesGiven.map((data) => {
+      for (let i = 0; i < totalCountries.length; i += 1) {
+        if (data === totalCountries[i].code) {
+          final.push(totalCountries[i].country);
+          break;
         }
-        return final;
-      });
+      }
+      return final;
+    });
     return final;
   });
