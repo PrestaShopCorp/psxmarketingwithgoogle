d2c632995a3ecc8023c51b4b8add054a24628dcc
Elise Griset <eGriset@PRESTAMAC-094.local>
2021-07-20 14:06:34 +0200

    test tunnel

---
+++ _dev/src/views/tunnel-product-feed.spec.ts
@@ -0,0 +1,42 @@
+/**
+ * @jest-environment jsdom
+ */
+
+// Import this file first to init mock on window
+import {commonOptions, cloneStore} from '@/../tests/init';
+
+// shallowMount creates the components without its children. Mount creates them all
+
+import {shallowMount} from '@vue/test-utils';
+import Vuex from 'vuex';
+import TunnelProductFeed from '@/views/tunnel-product-feed.vue';
+
+describe('tunnel-product-feed.vue', () => {
+  let actions;
+  let store;
+  beforeEach(() => {
+    actions = {
+      GET_PRODUCT_FEED_SETTINGS: jest.fn(),
+      GET_PRODUCT_FEED_SYNC_STATUS: jest.fn(),
+      GET_TOTAL_PRODUCTS: jest.fn(),
+    };
+    store = new Vuex.Store({
+      modules: {
+        productFeed: {
+          actions,
+          namespaced: true,
+        },
+      },
+    });
+  });
+
+  it('dispatches actions before Create', () => {
+    const wrapper = shallowMount(TunnelProductFeed, {
+      ...commonOptions,
+      store,
+    });
+    expect(actions.GET_PRODUCT_FEED_SETTINGS).toHaveBeenCalled();
+    expect(actions.GET_PRODUCT_FEED_SYNC_STATUS).toHaveBeenCalled();
+    expect(actions.GET_TOTAL_PRODUCTS).toHaveBeenCalled();
+  });
+});
