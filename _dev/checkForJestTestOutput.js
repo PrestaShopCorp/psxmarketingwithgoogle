050444e3a690dac23fd8da4ff9c232de12bb0c24
Amaury Hanser <amaury.hanser@gmail.com>
2021-07-20 14:46:58 +0200

    Updates package.json to tests for .jest-test-results.json

---
+++ _dev/checkForJestTestOutput.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+
+if (!fs.existsSync('.jest-test-results.json')) {
+  throw new Error(`
+    .jest-test-results.json not found
+    You need to run the tests first by using this command:
+    npm run test:generate-output
+
+    `,
+  );
+} else {
+  console.log(`
+    .jest-test-results.json exists
+    Proceed script execution
+
+    `,
+  );
+}
