c739f25ab12e624ad61a65cf14ee89b8e8c52d9f
Elise Griset <eGriset@PRESTAMAC-094.local>
2021-07-23 18:10:45 +0200

    add getter into action to get targetCountries

--- _dev/src/store/modules/product-feed/actions.ts
+++ _dev/src/store/modules/product-feed/actions.ts
@@ -106,13 +106,15 @@ export default {
     }
   },
 
-  async [ActionsTypes.SEND_PRODUCT_FEED_SETTINGS]({state, rootState, commit}) {
+  async [ActionsTypes.SEND_PRODUCT_FEED_SETTINGS]({
+    state, rootState, getters, commit,
+  }) {
     const productFeedSettings = state.settings;
+    const targetCountries = getters.GET_ACTIVE_COUNTRIES;
     const newSettings = {
       autoImportTaxSettings: productFeedSettings.autoImportTaxSettings,
       autoImportShippingSettings: productFeedSettings.autoImportShippingSettings,
-      targetCountries: productFeedSettings.targetCountries
-      ?? state.psxMktgWithGoogleActiveCountries,
+      targetCountries,
       shippingSettings: productFeedSettings.shippingSettings,
       attributeMapping: {
         exportProductsWithShortDescription:
