b4a1aaed85bcaa73b26d3bc1ffbccabfb8bc9198
Elise Griset <eGriset@PRESTAMAC-094.local>
2021-07-23 08:10:27 +0200

    unit testing

--- _dev/tests/init.ts
+++ _dev/tests/init.ts
@@ -2,15 +2,8 @@ import {config, createLocalVue} from '@vue/test-utils';
 import Vuex from 'vuex';
 import translations from '../.storybook/translations.json';
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-let windowSpy;
-=======
+
 let windowSpy; 
->>>>>>> ee4e6b4d (unit testing)
-=======
-let windowSpy;
->>>>>>> ec1796a1 (test error with vue showdown)
 export let localVue;
 const defaultLocale = 'en';
 
@@ -44,13 +37,6 @@ config.mocks.$i18n = {
   t: config.mocks.$t,
 };
 
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 export default {config};
 export {cloneStore} from './store';
-=======
-export {cloneStore} from './store';
->>>>>>> ee4e6b4d (unit testing)
-=======
-export {cloneStore} from './store';
->>>>>>> ec1796a1 (test error with vue showdown)
