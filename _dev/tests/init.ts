e0de4c288363e6a5cb3ce311c0c2397da9f714fe
Elise Griset <eGriset@PRESTAMAC-094.local>
2021-07-26 15:03:01 +0200

    remove comment

--- _dev/tests/init.ts
+++ _dev/tests/init.ts
@@ -3,7 +3,7 @@ import Vuex from 'vuex';
 import translations from '../.storybook/translations.json';
 
 let windowSpy;
-let localVue; // eslint-disable-line no-mutable-exports
+let localVue; 
 const defaultLocale = 'en';
 
 beforeEach(() => {
