5638b9d6d4a89fe0ed03d9f03001fcc12402c065
Amaury Hanser <amaury.hanser@gmail.com>
2021-07-30 15:00:39 +0200

    adds tests for click event

--- _dev/src/components/commons/stepper.spec.ts
+++ _dev/src/components/commons/stepper.spec.ts
@@ -8,30 +8,48 @@ import config, {cloneStore} from '@/../tests/init';
 
 import {shallowMount} from '@vue/test-utils';
 import Stepper from '@/components/commons/stepper.vue';
-
-const propsData = {
-  activeStep: 3,
-  steps: [
-    {
-      title: 'Shipping settings',
-    },
-    {
-      title: 'Export rules',
-    },
-    {
-      title: 'Attribute mapping',
-    },
-    {
-      title: 'Summary',
-    },
-  ],
-};
+import {state} from '../../store/modules/product-feed/state';
 
 describe('stepper.vue', () => {
-  let methods;
+  const productFeedSettingsRoute = {
+    name: 'product-feed-settings',
+  };
+  const fooRoute = {
+    name: 'foo',
+  };
+
+  const mockRouter = {
+    push: jest.fn(),
+  };
+
+  const propsData = {
+    activeStep: 3,
+    steps: [
+      {
+        title: 'Shipping settings',
+      },
+      {
+        title: 'Export rules',
+      },
+      {
+        title: 'Attribute mapping',
+      },
+      {
+        title: 'Summary',
+      },
+    ],
+  };
+
+  let mutations;
+  let store;
   beforeEach(() => {
-    methods = {
-      handleClick: jest.fn(),
+    mutations = {
+      SET_ACTIVE_CONFIGURATION_STEP: jest.fn(),
+    };
+    store = cloneStore();
+    store.modules.productFeed.mutations = {
+      ...store.modules.productFeed.mutations,
+      ...mutations,
     };
   });
 
@@ -39,7 +57,7 @@ describe('stepper.vue', () => {
     const wrapper = shallowMount(Stepper, {
       propsData,
       ...config,
-      store: new Vuex.Store(cloneStore()),
+      store: new Vuex.Store(store),
     });
 
     // Check if all steps before the active one have the class .complete
@@ -50,7 +68,7 @@ describe('stepper.vue', () => {
     const wrapper = shallowMount(Stepper, {
       propsData,
       ...config,
-      store: new Vuex.Store(cloneStore()),
+      store: new Vuex.Store(store),
     });
 
     // Check if all steps before the active one are links
@@ -61,10 +79,42 @@ describe('stepper.vue', () => {
     const wrapper = shallowMount(Stepper, {
       propsData,
       ...config,
-      store: new Vuex.Store(cloneStore()),
+      store: new Vuex.Store(store),
     });
 
     // Check if the active step has the class .active
     expect(wrapper.findAll('li').at(2).classes()).toContain('active');
+  });
+
+  it('test mutation commited when we click on a previous step on product-feed-settings route', async () => {
+    const wrapper = shallowMount(Stepper, {
+      mocks: {
+        $route: productFeedSettingsRoute,
+        $router: mockRouter,
+      },
+      propsData,
+      ...config,
+      store: new Vuex.Store(store),
+    });
+
+    // Check if the mutation SET_ACTIVE_CONFIGURATION_STEP has been called
+    await wrapper.findAll('a').at(0).trigger('click');
+    expect(mutations.SET_ACTIVE_CONFIGURATION_STEP).toHaveBeenCalledWith(state, 1);
+  });
+
+  it('test event emited when we click on a previous step on any other page', async () => {
+    const wrapper = shallowMount(Stepper, {
+      mocks: {
+        $route: fooRoute,
+        $router: mockRouter,
+      },
+      propsData,
+      ...config,
+      store: new Vuex.Store(store),
+    });
+
+    // Check clicking on a previous step is emiting the event changeStep
+    await wrapper.findAll('a').at(0).trigger('click');
+    expect(wrapper.emitted('changeStep')).toBeTruthy();
   });
 });
