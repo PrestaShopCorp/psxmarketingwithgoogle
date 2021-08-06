e2ebb58304813dbf1f4a4fb815f7c26773778aa4
Poulinho <poulai_a@etna-alternance.net>
2021-08-06 15:49:26 +0200

    fix storybook

--- _dev/stories/product-feed-table-status-details.stories.ts
+++ _dev/stories/product-feed-table-status-details.stories.ts
@@ -1,4 +1,5 @@
 import ProductFeedTableStatusDetails from '../src/components/product-feed-page/product-feed-table-status-details.vue'
+import {initialStateApp} from '../.storybook/mock/state-app';
 
 export default {
   title: 'Product feed page/Table Status Details',
@@ -11,6 +12,9 @@ const Template = (args, { argTypes }) => ({
   props: Object.keys(argTypes),
   components: { ProductFeedTableStatusDetails },
   template: '<div><ProductFeedTableStatusDetails v-bind="$props" /></div>',
+  beforeMount(this: any) {
+    this.$store.state.app = Object.assign({}, initialStateApp);
+  },
 });
 
 export const TableStatusDetails:any = Template.bind({});
