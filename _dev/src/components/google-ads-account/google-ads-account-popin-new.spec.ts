0ee25a1a7829f6e9588cf404739311eaec09722c
Amaury Hanser <amaury.hanser@gmail.com>
2021-08-18 15:25:55 +0200

    Tests stepper active step

--- _dev/src/components/google-ads-account/google-ads-account-popin-new.spec.ts
+++ _dev/src/components/google-ads-account/google-ads-account-popin-new.spec.ts
@@ -7,36 +7,51 @@
  import {cloneStore} from '@/../tests/init';
 
  import {mount, shallowMount} from '@vue/test-utils';
- import GoogleAccountPopinNew from '@/components/google-ads-account/google-ads-account-popin-new.vue';
- import PsModal from '@/components/commons/ps-modal.vue';
- import Stepper from '@/components/commons/stepper.vue';
-//  import {
-//   StepOne,
-//   StepTwo,
-//   StepThree,
-//  } from '@/../stories/google-ads-account-popin-new.stories';
-import {
-  googleAccountConnected,
-} from "@/../.storybook/mock/google-account";
+ import GoogleAdsAccountPopinNew from '@/components/google-ads-account/google-ads-account-popin-new.vue';
+ import {
+   googleAccountConnected,
+ } from "@/../.storybook/mock/google-account";
 
- describe('google-ads-account.vue / step 1', () => {
-   it('shows a link to create a new account', () => {
-     const wrapper = mount(GoogleAccountPopinNew, {
-       store: new Vuex.Store(cloneStore()),
-       propsData: {
-        visible: true,
-        stepActiveData: 1,
-        user: Object.assign({}, googleAccountConnected),
-       },
-       stubs: {
-        VueShowdown: true,
-        PsModal,
-        Stepper,
-      },
-     });
+const VBTooltip = jest.fn();
 
-     console.log(wrapper.html())
+const wrapper = mount(GoogleAdsAccountPopinNew, {
+  store: new Vuex.Store(cloneStore()),
+  propsData: {
+   visible: true,
+   user: Object.assign({}, googleAccountConnected),
+  },
+  stubs: {
+   VueShowdown: true,
+   BButton: true,
+ },
+  directives: {
+    'b-tooltip': VBTooltip,
+  },
+});
 
-     expect(wrapper.findAll('.ps_gs-stepper-step').at(0).attributes('aria-current')).toEqual('step');
-    });
- });
+describe('google-ads-account.vue / step 1', () => {
+  it('step 1 is active in the stepper', async () => {
+    await wrapper.setData({ stepActiveData: 1 })
+
+    expect(wrapper.findAll('.ps_gs-stepper-step').at(0).attributes('aria-current')).toEqual('step');
+    expect(wrapper.findAll('.ps_gs-stepper-step').at(1).attributes('aria-current')).toBeUndefined();
+   });
+});
+
+describe('google-ads-account.vue / step 2', () => {
+  it('step 2 is active in the stepper', async () => {
+    await wrapper.setData({ stepActiveData: 2 })
+
+    expect(wrapper.findAll('.ps_gs-stepper-step').at(1).attributes('aria-current')).toEqual('step');
+    expect(wrapper.findAll('.ps_gs-stepper-step').at(0).attributes('aria-current')).toBeUndefined();
+   });
+});
+
+describe('google-ads-account.vue / step 3', () => {
+  it('step 3 is active in the stepper', async () => {
+    await wrapper.setData({ stepActiveData: 3 })
+
+    expect(wrapper.findAll('.ps_gs-stepper-step').at(2).attributes('aria-current')).toEqual('step');
+    expect(wrapper.findAll('.ps_gs-stepper-step').at(0).attributes('aria-current')).toBeUndefined();
+   });
+});
