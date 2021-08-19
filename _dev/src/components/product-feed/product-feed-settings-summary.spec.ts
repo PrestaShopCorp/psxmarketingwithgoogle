50176e45a862365b3f3a8e7c75ead2b6be0ec866
Elise Griset <eGriset@PRESTAMAC-094.local>
2021-08-19 15:48:55 +0200

    remove filters

--- _dev/src/components/product-feed/product-feed-settings-summary.spec.ts
+++ _dev/src/components/product-feed/product-feed-settings-summary.spec.ts
@@ -4,7 +4,7 @@
 import Vuex from 'vuex';
 
 // Import this file first to init mock on window
-import config, {localVue, cloneStore} from '@/../tests/init';
+import config, {localVue, cloneStore, filters} from '@/../tests/init';
 import {mount, shallowMount} from '@vue/test-utils';
 import ProductFeedSettingsSummary from '@/components/product-feed/product-feed-settings-summary.vue';
 import ProductFeedCardNextSyncCard from '@/components/product-feed/product-feed-card-next-sync-card.vue';
@@ -17,32 +17,17 @@ import {
 
 describe('product-feed-settings-summary.vue', () => {
   it('shows datas', () => {
-    const timeConverterToDate = jest.fn();
-    localVue.filter('timeConverterToDate', timeConverterToDate);
-    const timeConverterToHour = jest.fn();
-    localVue.filter('timeConverterToHour', timeConverterToHour);
-    const changeCountryCodeToName = jest.fn();
-    changeCountryCodeToName.mockImplementation(() => []);
-    localVue.filter('changeCountryCodeToName', changeCountryCodeToName);
     const wrapper = shallowMount(ProductFeedSettingsSummary, {
       propsData: Summary.args,
       beforeMount: Summary.args.beforeMount,
       localVue,
       store: new Vuex.Store(cloneStore()),
     });
-    changeCountryCodeToName.mockImplementation(() => []);
-    localVue.filter('changeCountryCodeToName', changeCountryCodeToName);
-    expect(changeCountryCodeToName).toHaveBeenCalledTimes(1);
+
+    expect(filters.changeCountryCodeToName).toHaveBeenCalledTimes(1);
     expect(wrapper.findComponent(ProductFeedCardNextSyncCard).exists()).toBeTruthy();
   });
   it('shows button cancel and triggers previous step on click', async () => {
-    const timeConverterToDate = jest.fn();
-    localVue.filter('timeConverterToDate', timeConverterToDate);
-    const timeConverterToHour = jest.fn();
-    localVue.filter('timeConverterToHour', timeConverterToHour);
-    const changeCountryCodeToName = jest.fn();
-    changeCountryCodeToName.mockImplementation(() => []);
-    localVue.filter('changeCountryCodeToName', changeCountryCodeToName);
     const wrapper = shallowMount(ProductFeedSettingsSummary, {
       propsData: Summary.args,
       beforeMount: Summary.args.beforeMount,
