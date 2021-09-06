dbd078f868303d455982597a6895b2b234871a94
Amaury Hanser <amaury.hanser@gmail.com>
2021-09-06 18:22:31 +0200

    Updates horizontal logo

--- _dev/src/components/landing-page/landing-page-header.vue
+++ _dev/src/components/landing-page/landing-page-header.vue
@@ -1,6 +1,6 @@
 <template>
   <header class="ps_gs-landingpage-header d-flex flex-column flex-md-row">
-    <figure class="ps_gs-landingpage-header__figure order-md-2 flex-shrink-0 ml-md-3">
+    <figure class="ps_gs-landingpage-header__figure order-md-2 d-none d-md-block flex-shrink-0 ml-md-3">
       <img
         src="@/assets/images/prestashop-google-img.png"
         width="276"
@@ -14,21 +14,10 @@
         class="ps_gs-landingpage-header__title d-flex mr-3"
       >
         <img
-          src="@/assets/images/temp_target.svg"
-          width="48"
-          height="48"
-          class="img-fluid mr-3"
-          alt=""
+          src="@/assets/images/prestashop-google-logo.svg"
+          class="img-fluid mx-auto ml-md-0 mr-md-3 maxw-sm-420"
+          alt="logo"
         >
-        <span
-          style="color:#8e95a1; font-weight:600; font-size:1.5rem"
-          class="d-flex align-items-center flex-wrap"
-        >
-          <span
-            style="color:#27206b;"
-            class="mr-2"
-          >PrestaShop</span> <span>Marketing with Google</span>
-        </span>
         <span class="sr-only">{{ $t('landingPage.header.title') }}</span>
       </h2>
       <p class="ps_gs-landingpage-header__text w-100">
