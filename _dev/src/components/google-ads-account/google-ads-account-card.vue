438f1a4faecd61944d8309947ad99635d6cbe800
Amaury Hanser <amaury.hanser@gmail.com>
2021-09-07 12:11:59 +0200

    Fix wrongly rounded icon

--- _dev/src/components/google-ads-account/google-ads-account-card.vue
+++ _dev/src/components/google-ads-account/google-ads-account-card.vue
@@ -11,7 +11,7 @@
         class="d-flex align-items-center"
       >
         <img
-          class="mr-3 rounded-circle"
+          class="mr-3"
           src="@/assets/images/Google-Commercial-icon-grey.svg"
           width="40"
           height="40"
