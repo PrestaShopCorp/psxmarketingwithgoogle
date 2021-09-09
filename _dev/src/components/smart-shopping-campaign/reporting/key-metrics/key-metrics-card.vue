70bad6e5a8cb8be3a0e961b541f1a20a43bd48e1
Amaury Hanser <amaury.hanser@gmail.com>
2021-09-09 15:40:13 +0200

    Adds first init of campaign-performance-table

--- _dev/src/components/smart-shopping-campaign/reporting/key-metrics/key-metrics-card.vue
+++ _dev/src/components/smart-shopping-campaign/reporting/key-metrics/key-metrics-card.vue
@@ -36,44 +36,17 @@
           :tooltip="$t(`keymetrics.${kpi.type}Tooltip`)"
         />
       </KeyMetricsKpiCardGroup>
-      <section class="mt-5 pt-2">
-        <div class="d-flex align-items-center justify-content-between flex-wrap">
-          <h4 class="font-weight-600 ps_gs-fz-16 mb-2 flex-shrink-0 mr-3">
-            {{ $t('keymetrics.dailyResultTitle') }}
-          </h4>
-          <b-dropdown
-            id="dailyResultKpi"
-            ref="dailyResultKpi"
-            :text="$t(`keymetrics.${selectedDailyResultKpi}`)"
-            variant=" "
-            class="ps-dropdown psxmarketingwithgoogle-dropdown bordered maxw-sm-250 mb-2"
-            size="sm"
-            menu-class="ps-dropdown"
-            no-flip
-          >
-            <b-dropdown-item
-              v-for="kpi in kpis"
-              :key="kpi.type"
-              link-class="d-flex flex-wrap flex-md-nowrap align-items-center px-3"
-              @click="selectedDailyResultKpi = kpi.type"
-            >
-              {{ $t(`keymetrics.${kpi.type}`) }}
-            </b-dropdown-item>
-          </b-dropdown>
-        </div>
-        <div>
-          placeholder chart
-        </div>
-        <div class="text-right text-primary mt-3">
-          <a
-            href="//google.com"
-            target="_blank"
-            class="d-inline-block"
-          >
-            See on Google Ads
-          </a>
-        </div>
-      </section>
+      <KeyMetricsChartWrapper :kpis="kpis"/>
+      <div class="text-right text-primary mt-3">
+        <!-- TODO / adds link -->
+        <a
+          href="//google.com"
+          target="_blank"
+          class="d-inline-block"
+        >
+          {{ $t('cta.seeOnGoogleAds') }}
+        </a>
+      </div>
     </b-card>
 </template>
 
@@ -81,6 +54,7 @@
 import KeyMetricsPeriodSelector from './key-metrics-period-selector.vue';
 import KeyMetricsKpiCardGroup from './key-metrics-kpi-card-group.vue';
 import KeyMetricsKpiCard from './key-metrics-kpi-card.vue';
+import KeyMetricsChartWrapper from './key-metrics-chart-wrapper.vue';
 
 export default {
   name: 'KeyMetricsCard',
@@ -88,6 +62,7 @@ export default {
     KeyMetricsPeriodSelector,
     KeyMetricsKpiCardGroup,
     KeyMetricsKpiCard,
+    KeyMetricsChartWrapper,
   },
   data() {
     return {
