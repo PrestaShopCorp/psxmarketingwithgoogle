c6c3cee0006fdc32eb5d28f8cca264727402e3c0
Amaury Hanser <amaury.hanser@gmail.com>
2021-09-20 13:46:19 +0200

    Updates campaign list markup

--- _dev/src/components/smart-shopping-campaign/smart-shopping-campaign-table-list.vue
+++ _dev/src/components/smart-shopping-campaign/smart-shopping-campaign-table-list.vue
@@ -1,9 +1,9 @@
 <template>
   <div>
-    <div class="d-flex justify-content-between mb-3 rounded-top">
-      <h3 class="ps_gs-fz-20 font-weight-600">{{ $t('smartShoppingCampaignList.tableTitle') }}</h3>
+    <div class="d-flex flex-wrap flex-md-nowrap justify-content-between mb-md-3 rounded-top">
+      <h3 class="order-2 order-md-1 ps_gs-fz-20 font-weight-600">{{ $t('smartShoppingCampaignList.tableTitle') }}</h3>
       <div
-        class="mx-auto d-flex-md mr-md-0 flex-md-shrink-0 text-center"
+        class="order-1 order-md-2 ml-auto d-flex-md mr-md-0 mb-2 mt-n3 mt-md-0 flex-md-shrink-0 text-center"
       >
         <b-button
           size="sm"
@@ -24,70 +24,187 @@
     <ReportingTableHeader
       :title="'12 campaigns'"
     />
-    <b-table-simple
-      id="table-filters-performance"
-      class="ps_gs-table-products mb-3"
-      :table-class="{'border-bottom-0': loading}"
-      variant="light"
-      responsive="xl"
-    >
-      <b-thead>
-        <b-tr>
-          <b-th
-            v-for="(type, index) in campaignHeaderList"
-            :key="type"
-            class="font-weight-600"
-            :class="{'b-table-sticky-column b-table-sticky-column--invisible': index === 0}"
+    <div>
+      <b-table-simple
+        id="table-filters-performance"
+        class="ps_gs-table-products mb-0"
+        :table-class="{'border-bottom-0': loading}"
+        variant="light"
+        responsive="xl"
+      >
+        <b-thead>
+          <b-tr>
+            <b-th
+              v-for="(type, index) in campaignHeaderList"
+              :key="type"
+              class="font-weight-600"
+              :class="{'b-table-sticky-column b-table-sticky-column--invisible': index === 0}"
+            >
+              <div class="flex align-items-center text-nowrap">
+                <b-button
+                  v-if="hasSorting(type)"
+                  @click="sortByType(type)"
+                  variant="invisible"
+                  class="p-0 border-0"
+                >
+                  <span>{{ $t(`campaigns.labelCol.${type}`) }}</span>
+                  <template v-if="queryOrderDirection[type] === 'ASC'">
+                    <i class="material-icons ps_gs-fz-14">expand_more</i>
+                    <span class="sr-only">{{ $t('cta.clickToSortAsc') }}</span>
+                  </template>
+                  <template v-else>
+                    <i class="material-icons ps_gs-fz-14">expand_less</i>
+                    <span class="sr-only">{{ $t('cta.clickToSortDesc') }}</span>
+                  </template>
+                </b-button>
+                <span v-else>
+                  {{ $t(`campaigns.labelCol.${type}`) }}
+                </span>
+                <b-button
+                  v-if="hasToolTip(type)"
+                  variant="invisible"
+                  v-b-tooltip:psxMktgWithGoogleApp
+                  :title="$t(`campaigns.tooltipCol.${type}`)"
+                  class="p-0 mt-0 ml-1 border-0 d-inline-flex align-items-center"
+                >
+                  <i class="material-icons ps_gs-fz-14 text-secondary">error_outline</i>
+                </b-button>
+              </div>
+            </b-th>
+          </b-tr>
+        </b-thead>
+        <b-tbody class="bg-white">
+          <SmartShoppingCampaignTableListRow
+            v-for="campaign in campaignList"
+            :key="campaign.name"
+            :campaign="campaign"
+          />
+          <b-tr v-if="loading">
+            <b-td
+              colspan="7"
+              class="ps_gs-table-products__loading-slot"
+            >
+              <i class="ps_gs-table-products__spinner">loading</i>
+            </b-td>
+          </b-tr>
+        </b-tbody>
+      </b-table-simple>
+      <div class="ps_gs-table-controls d-flex flex-wrap flex-md-nowrap
+        align-items-center bg-white pt-2 px-3 pb-3 pt-md-3">
+        <div class="ps_gs-table-controls__products-shown d-md-flex align-items-center d-none">
+          <span>Show:</span>
+          <b-dropdown
+            id="filterQuantityToShow"
+            ref="filterQuantityToShow"
+            :text="selectedFilterQuantityToShow"
+            variant=" "
+            class="flex-grow-1 ps-dropdown psxmarketingwithgoogle-dropdown bordered w-auto ml-2"
+            menu-class="ps-dropdown"
+
+            size="sm"
           >
-            <div class="flex align-items-center text-nowrap">
-              <b-button
-                v-if="hasSorting(type)"
-                @click="sortByType(type)"
-                variant="invisible"
-                class="p-0 border-0"
+            <b-dropdown-item
+              v-for="(option, index) in ['10', '20', '50', '100']"
+              :key="index"
+              @click="selectedFilterQuantityToShow = option"
+            >
+              {{ option }}
+            </b-dropdown-item>
+          </b-dropdown>
+        </div>
+        <nav
+          aria-label="Table pagination"
+          class="ps_gs-table-controls__pagination mx-md-auto"
+        >
+          <ul class="pagination mb-0">
+            <li class="page-item previous">
+              <a
+                class="page-link"
+                href="#"
+                aria-label="Previous"
               >
-                <span>{{ $t(`campaigns.labelCol.${type}`) }}</span>
-                <template v-if="queryOrderDirection[type] === 'ASC'">
-                  <i class="material-icons ps_gs-fz-14">expand_more</i>
-                  <span class="sr-only">{{ $t('cta.clickToSortAsc') }}</span>
-                </template>
-                <template v-else>
-                  <i class="material-icons ps_gs-fz-14">expand_less</i>
-                  <span class="sr-only">{{ $t('cta.clickToSortDesc') }}</span>
-                </template>
-              </b-button>
-              <span v-else>
-                {{ $t(`campaigns.labelCol.${type}`) }}
-              </span>
-              <b-button
-                v-if="hasToolTip(type)"
-                variant="invisible"
-                v-b-tooltip:psxMktgWithGoogleApp
-                :title="$t(`campaigns.tooltipCol.${type}`)"
-                class="p-0 mt-0 ml-1 border-0 d-inline-flex align-items-center"
+                <span class="sr-only">Previous</span>
+              </a>
+            </li>
+            <li class="page-item">
+              <a
+                class="page-link"
+                href="#"
+              >1</a>
+            </li>
+            <li class="page-item disabled">
+              <a
+                class="page-link"
+                href="#"
+              >...</a>
+            </li>
+            <li class="page-item">
+              <a
+                class="page-link"
+                href="#"
+              >6</a>
+            </li>
+            <li
+              class="page-item active"
+              aria-current="page"
+            >
+              <a
+                class="page-link"
+                href="#"
+              >7</a>
+            </li>
+            <li class="page-item">
+              <a
+                class="page-link"
+                href="#"
+              >8</a>
+            </li>
+            <li class="page-item">
+              <a
+                class="page-link"
+                href="#"
+              >9</a>
+            </li>
+            <li class="page-item disabled">
+              <a
+                class="page-link"
+                href="#"
+              >...</a>
+            </li>
+            <li class="page-item">
+              <a
+                class="page-link"
+                href="#"
+              >16</a>
+            </li>
+            <li class="page-item next">
+              <a
+                class="page-link"
+                href="#"
+                aria-label="Next"
               >
-                <i class="material-icons ps_gs-fz-14 text-secondary">error_outline</i>
-              </b-button>
-            </div>
-          </b-th>
-        </b-tr>
-      </b-thead>
-      <b-tbody class="bg-white">
-        <SmartShoppingCampaignTableListRow
-          v-for="campaign in campaignList"
-          :key="campaign.name"
-          :campaign="campaign"
-        />
-        <b-tr v-if="loading">
-          <b-td
-            colspan="7"
-            class="ps_gs-table-products__loading-slot"
+                <span class="sr-only">Next</span>
+              </a>
+            </li>
+          </ul>
+        </nav>
+        <div class="ps_gs-table-controls__go-to d-flex align-items-center">
+          <span class="flex-shrink-0">Go to page:</span>
+          <b-form-input
+            class="ml-2 maxw-sm-72 flex-grow-0"
+            type="text"
+            size="sm"
+          />
+          <b-button
+            variant="primary"
+            class="ml-2"
+            size="sm"
           >
-            <i class="ps_gs-table-products__spinner">loading</i>
-          </b-td>
-        </b-tr>
-      </b-tbody>
-    </b-table-simple>
+            Go
+          </b-button>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -106,6 +223,7 @@ export default {
   data() {
     return {
       loading: false,
+      selectedFilterQuantityToShow: '10',
     };
   },
   computed: {
