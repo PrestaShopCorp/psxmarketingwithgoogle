78438424199bfcd8b381657cafeed019283b0cf5
Amaury Hanser <amaury.hanser@gmail.com>
2021-09-21 14:57:09 +0200

    Updates mock with campaign list and fix intersection observer

--- _dev/stories/campaigns-performance-table.stories.ts
+++ _dev/stories/campaigns-performance-table.stories.ts
@@ -1,4 +1,7 @@
 import CampaignsPerformanceTable from '../src/components/smart-shopping-campaign/reporting/campaigns-performance/campaigns-performance-table.vue';
+import {campaignsPerformanceList} from '../.storybook/mock/campaigns-list.js';
+import {googleAdsAccountChosen} from '../.storybook/mock/google-ads';
+
 export default {
   title: 'Reporting/Campaigns Performance',
   component: CampaignsPerformanceTable,
@@ -12,8 +15,14 @@ const Template = (args, { argTypes }) => ({
       <CampaignsPerformanceTable v-bind="$props"/>
     </div>
   `,
+  beforeMount : args.beforeMount,
 });
 
 export const CampaignsPerformance:any = Template.bind({});
 CampaignsPerformance.args = {
+  beforeMount(this: any) {
+    this.$store.state.googleAds = Object.assign({}, googleAdsAccountChosen);
+    this.$store.state.smartShoppingCampaigns.reporting.results.campaignsPerformancesSection.campaignsPerformanceList = Object.assign({}, campaignsPerformanceList);
+    this.$store.state.smartShoppingCampaigns.reporting.results.campaignsPerformancesSection.nextPageToken = Object.assign({}, 'fooBar');
+  },
 }
