6be7824c8b325f2028abb678822957149b188304
Amaury Hanser <amaury.hanser@gmail.com>
2021-09-21 10:57:25 +0200

    Updates date range picker options

--- _dev/src/components/commons/range-picker.vue
+++ _dev/src/components/commons/range-picker.vue
@@ -1,11 +1,15 @@
 <template>
-  <div
+  <b-button
     id="datepicker"
-  />
+  >
+    tutu
+  </b-button>
 </template>
 
 <script>
 import LitePicker from "litepicker";
+import 'litepicker/dist/plugins/keyboardnav';
+import 'litepicker/dist/plugins/mobilefriendly';
 
 export default {
   name: 'RangePicker',
@@ -18,21 +22,26 @@ export default {
   mounted() {
     new Litepicker({
       element: document.getElementById('datepicker'),
-      singleMode: false,
+      // singleMode: false,
       tooltipText: {
         one: 'day',
         other: 'days'
       },
       tooltipNumber: (totalDays) => {
         return totalDays - 1;
-      }
+      },
+      plugins: ['keyboardnav', 'mobilefriendly'],
+      // autoApply: true,
+      singleMode: false,
+      numberOfColumns: 2,
+      numberOfMonths: 2,
+      showWeekNumbers: true,
+      mobileFriendly: true,
+      minDate: new Date(),
+      dropdowns: { months: true, years: true },
+      lang: window.i18nSettings.languageLocale.split('-')[0],
+      resetButton: true,
     });
   },
 };
 </script>
-
-<style lang="sass">
-  .litepicker {
-    display: block !important
-  }
-</style>
