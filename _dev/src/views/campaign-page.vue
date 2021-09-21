4c04436636a9e7ec69ba2fb7ec02f6dbebd17af7
Elise Griset <eGriset@prestamac-094.prestashop.net>
2021-09-21 09:44:31 +0200

    Toast creation camapign

--- _dev/src/views/campaign-page.vue
+++ _dev/src/views/campaign-page.vue
@@ -6,10 +6,20 @@
     />
     <smart-shopping-campaign-creation
       v-if="$route.name === 'campaign-creation'"
+      @campaignCreated="onCampaignHasBeenCreated"
     />
     <SSCPopinActivateTracking
       ref="SSCPopinActivateTracking"
     />
+    <PsToast
+      v-if="campaignCreated"
+      variant="success"
+      @hidden="toastIsClosed"
+      :visible="campaignCreated"
+      toaster="b-toaster-top-right"
+    >
+      <p>{{ insideToast }}</p>
+    </PsToast>
   </div>
 </template>
 
@@ -17,16 +27,31 @@
 import CampaignCard from '../components/smart-shopping-campaigns/campaign-card.vue';
 import SSCPopinActivateTracking from '../components/smart-shopping-campaigns/ssc-popin-activate-tracking.vue';
 import SmartShoppingCampaignCreation from '../components/smart-shopping-campaign-creation/smart-shopping-campaign-creation.vue';
+import PsToast from '../components/commons/ps-toast';
 
 export default {
   components: {
     CampaignCard,
     SmartShoppingCampaignCreation,
     SSCPopinActivateTracking,
+    PsToast,
+  },
+
+  data() {
+    return {
+      campaignCreated: false,
+    };
   },
   computed: {
     googleAdsIsChosen() {
       return this.$store.getters['googleAds/GET_GOOGLE_ADS_ACCOUNT_CHOSEN'];
+    },
+
+    insideToast() {
+      if (this.campaignCreated) {
+        return this.$t('toast.campaignCreatedSuccess');
+      }
+      return '';
     },
   },
   methods: {
@@ -40,6 +65,12 @@ export default {
       this.$bvModal.show(
         this.$refs.SSCPopinActivateTracking.$refs.modal.id,
       );
+    },
+    onCampaignHasBeenCreated() {
+      this.campaignCreated = true;
+    },
+    toastIsClosed() {
+      this.campaignCreated = false;
     },
   },
   mounted() {
