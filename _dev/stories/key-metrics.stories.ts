0e9763c453da7991a1a373d12a362d7af20d2e15
v4lux <juliengissinger@gmail.com>
2021-09-22 14:22:46 +0200

    fix: hydrate storybook state with mock data

--- _dev/stories/key-metrics.stories.ts
+++ _dev/stories/key-metrics.stories.ts
@@ -1,4 +1,5 @@
 import KeyMetricsBlock from '../src/components/smart-shopping-campaign/reporting/key-metrics/key-metrics-block.vue'
+import reportingDailyResults from '../.storybook/mock/reporting-kpis';
 import {googleAdsAccountChosen} from '../.storybook/mock/google-ads.js';
 
 export default {
@@ -20,6 +21,7 @@ const Template = (args, { argTypes }) => ({
 export const KeyMetrics:any = Template.bind({});
 KeyMetrics.args = {
   beforeMount(this: any) {
-      this.$store.state.googleAds = Object.assign({}, googleAdsAccountChosen);
+    this.$store.state.googleAds = Object.assign({}, googleAdsAccountChosen);
+    this.$store.state.smartShoppingCampaigns.reporting.results.dailyResultChart.dailyResultList = reportingDailyResults;
   }
 }
