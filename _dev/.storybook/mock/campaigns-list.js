e8242be287499ca3d0e6dbd0c88410b3d5e990c0
Amaury Hanser <amaury.hanser@gmail.com>
2021-09-23 16:58:14 +0200

    Fix mock for ssc summary

--- _dev/.storybook/mock/campaigns-list.js
+++ _dev/.storybook/mock/campaigns-list.js
@@ -13,6 +13,12 @@ export const productsPartitionsPerformancesSectionEmpty = {
   nextPageToken: null,
 }
 
+export const campaignsEmpty = [];
+
+export const campaigns = [
+  {"campaignName":"qweqwe","startDate":"2021-09-23","endDate":"2037-12-30","targetCountry":"FR","dailyBudget":1,"status":"ELIGIBLE","currencyCode":"EUR","productFilters":[]}
+];
+
 export const campaignsPerformanceSection = {
   campaignsPerformanceList: [
     {
--- _dev/stories/smart-shopping-campaign-table-list.stories.ts
+++ _dev/stories/smart-shopping-campaign-table-list.stories.ts
@@ -1,4 +1,5 @@
 import SmartShoppingCampaignTableList from '../src/components/smart-shopping-campaign/smart-shopping-campaign-table-list.vue'
+import {campaigns, campaignsEmpty} from '../.storybook/mock/campaigns-list.js';
 
 export default {
   title: 'Smart Shopping Campaign/Campaigns\'s list',
@@ -10,19 +11,30 @@ const Template = (args, { argTypes }) => ({
   components: { SmartShoppingCampaignTableList },
   template: `
     <div>
-      <SmartShoppingCampaignTableList ref="SmartShoppingCampaignTableList" />
+      <SmartShoppingCampaignTableList ref="SmartShoppingCampaignTableList" v-bind="$props" />
     </div>
   `,
   mounted: args.mounted,
+  beforeCreate: args.beforeCreate,
 });
 
 export const Table:any = Template.bind({});
 Table.args = {
+  beforeCreate() {
+    this.$store.state.smartShoppingCampaigns.campaigns = Object.assign([], campaigns);
+  },
+  loading: false,
 }
 
 export const Loading:any = Template.bind({});
 Loading.args = {
-  mounted() {
-    this.$refs.SmartShoppingCampaignTableList.$data.loading = true;
+  loading: true,
+}
+
+export const Empty:any = Template.bind({});
+Empty.args = {
+  beforeCreate() {
+    this.$store.state.smartShoppingCampaigns.campaigns = Object.assign([], campaignsEmpty);
   },
+  loading: true,
 }
