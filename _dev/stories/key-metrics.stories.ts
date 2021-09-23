dce6757b7d286b0109b1e1af1d480e6f0cabdc58
Amaury Hanser <amaury.hanser@gmail.com>
2021-09-23 11:00:57 +0200

    Updates kpi story with mocked data

--- _dev/stories/key-metrics.stories.ts
+++ _dev/stories/key-metrics.stories.ts
@@ -1,5 +1,5 @@
 import KeyMetricsBlock from '../src/components/smart-shopping-campaign/reporting/key-metrics/key-metrics-block.vue'
-import reportingDailyResults from '../.storybook/mock/reporting-kpis';
+import {reportingDailyResults, reportingDailyResultsEmpty, kpisResults} from '../.storybook/mock/reporting-kpis';
 import {googleAdsAccountChosen} from '../.storybook/mock/google-ads.js';
 
 export default {
@@ -30,10 +30,19 @@ ApiError.args = {
   },
 };
 
-export const KeyMetrics:any = Template.bind({});
-KeyMetrics.args = {
+export const WithResults:any = Template.bind({});
+WithResults.args = {
   beforeMount(this: any) {
     this.$store.state.googleAds = Object.assign({}, googleAdsAccountChosen);
     this.$store.state.smartShoppingCampaigns.reporting.results.dailyResultChart.dailyResultList = reportingDailyResults;
+    this.$store.state.smartShoppingCampaigns.reporting.results.kpis = Object.assign({}, kpisResults);
+  },
+};
+
+export const NoResults:any = Template.bind({});
+NoResults.args = {
+  beforeMount(this: any) {
+    this.$store.state.googleAds = Object.assign({}, googleAdsAccountChosen);
+    this.$store.state.smartShoppingCampaigns.reporting.results.dailyResultChart.dailyResultList = reportingDailyResultsEmpty;
   },
 };
