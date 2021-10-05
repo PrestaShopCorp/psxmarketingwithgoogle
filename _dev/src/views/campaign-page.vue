fddf379950983d6c92d5fdfed5da74d9e3d9a3ad
Elise Griset <elise.griset@prestashop.com>
2021-10-05 08:52:12 +0200

    modify campaign page

--- _dev/src/views/campaign-page.vue
+++ _dev/src/views/campaign-page.vue
@@ -75,6 +75,7 @@ export default {
         this.loading = false;
       });
       await this.$store.dispatch('googleAds/GET_GOOGLE_ADS_ACCOUNT');
+      await this.$store.dispatch('productFeed/GET_TOTAL_PRODUCTS');
       await this.$store.dispatch('productFeed/GET_PRODUCT_FEED_SYNC_STATUS');
       await this.$store.dispatch('productFeed/GET_PRODUCT_FEED_SETTINGS');
       await this.$store.dispatch('productFeed/GET_PRODUCT_FEED_SYNC_SUMMARY');
