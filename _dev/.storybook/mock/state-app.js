7cbbf4e10237593b152492ccc83c10ccbc5e8760
Amaury Hanser <amaury.hanser@gmail.com>
2021-10-14 16:10:32 +0200

    Removes commented lines

--- _dev/.storybook/mock/state-app.js
+++ _dev/.storybook/mock/state-app.js
@@ -1,10 +1,8 @@
 export const initialStateApp = {
     psxMktgWithGoogleApiUrl: '',
-    // psxMktgWithGoogleApiUrl: 'https://mock-googleshopping-api.psessentials-integration.net/rest/PS+Google+Shopping+all-in-one/1.0',
     psxMktgWithGoogleShopUrl: 'https://my-shop.com',
     psxMktgWithGoogleAdminUrl: 'https://my-shop.com/admin-dev',
     psxMktgWithGoogleAdminAjaxUrl: '/',
-    // psxMktgWithGoogleAdminAjaxUrl: 'https://mock-googleshopping-api.psessentials-integration.net/rest/Ajax+requests+to+shop+specification/1.0.0/',
     shopIdPsAccounts: '',
     tokenPsAccounts: '',
     psxMtgWithGoogleDefaultShopCountry: null,
