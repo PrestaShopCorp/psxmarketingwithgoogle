ddd53df3aa7438124e43140548d09b23c9a2d662
Thomas Nabord <thomas.nabord@prestashop.com>
2021-10-15 17:42:32 +0200

    Enable Sentry on production only

--- _dev/src/utils/Sentry.ts
+++ _dev/src/utils/Sentry.ts
@@ -2,23 +2,26 @@ import Vue from 'vue';
 import * as Sentry from '@sentry/vue';
 import store from '@/store';
 
-Sentry.init({
-  Vue,
-  // @ts-ignore
-  dsn: global.psxMktgWithGoogleDsnSentry,
-  tracesSampleRate: 1.0,
-  logErrors: true,
-  initialScope: {
-    tags: {
-      // @ts-ignore
-      'prestashop-version': store.state.app.psVersion,
-      // @ts-ignore
-      'module-version': store.state.app.psxMktgWithGoogleModuleVersion,
+// @ts-ignore
+if (store.state.app.psxMktgWithGoogleOnProductionEnvironment) {
+  Sentry.init({
+    Vue,
+    // @ts-ignore
+    dsn: global.psxMktgWithGoogleDsnSentry,
+    tracesSampleRate: 1.0,
+    logErrors: true,
+    initialScope: {
+      tags: {
+        // @ts-ignore
+        'prestashop-version': store.state.app.psVersion,
+        // @ts-ignore
+        'module-version': store.state.app.psxMktgWithGoogleModuleVersion,
+      },
+      user: {
+        id: window.shopIdPsAccounts ? window.shopIdPsAccounts.toString() : 'unknown',
+      },
     },
-    user: {
-      id: window.shopIdPsAccounts ? window.shopIdPsAccounts.toString() : 'unknown',
-    },
-  },
-  // @ts-ignore
-  release: `v${store.state.app.psxMktgWithGoogleModuleVersion}`,
-});
+    // @ts-ignore
+    release: `v${store.state.app.psxMktgWithGoogleModuleVersion}`,
+  });
+}
