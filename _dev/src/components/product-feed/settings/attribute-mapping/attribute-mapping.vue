8fac590d38855551c081f62dd2110c5807380f92
Amaury Hanser <amaury.hanser@gmail.com>
2021-10-27 16:14:40 +0200

    Updates fields

--- _dev/src/components/product-feed/settings/attribute-mapping/attribute-mapping.vue
+++ _dev/src/components/product-feed/settings/attribute-mapping/attribute-mapping.vue
@@ -1,13 +1,68 @@
 <template>
   <div>
-    <h3
-      class="ps_gs-fz-20 font-weight-600 mb-2"
-    >
-      Map mandatory Google attributes to your custom fields
-    </h3>
-    <p>
-      Google requires filling in a specific list of attributes depending on which product categories you sell. Map these attributes to your custom product fields to ensure your products are validated.
-    </p>
+    <b-form>
+      <h3
+        class="ps_gs-fz-20 font-weight-600 mb-2"
+      >
+        Map mandatory Google attributes to your custom fields
+      </h3>
+      <p>
+        Google requires filling in a specific list of attributes depending on which product categories you sell. Map these attributes to your custom product fields to ensure your products are validated.
+      </p>
+      <b-form-group
+        v-for="(category, index) in attributes"
+        :key="category"
+        :class="{'border-bottom': index !== attributes.length - 1}"
+        class="mb-3 pb-2"
+      >
+        <VueShowdown
+          v-if="category.legend"
+          :markdown="category.legend"
+          tag="legend"
+          class="bg-transparent border-0 ps_gs-fz-14 font-weight-normal mb-3"
+          :extensions="['no-p-tag']"
+        />
+        <b-form-row>
+          <b-col
+            v-for="field in category.fields"
+            :key="field.label"
+            :cols="12"
+            :md="6"
+          >
+            <b-form-group
+              :label="field.label"
+              :label-for="field.label"
+              label-class="font-weight-600"
+            >
+              <b-dropdown
+                :text="field.default"
+                variant=" "
+                class="maxw-sm-250 ps-dropdown psxmarketingwithgoogle-dropdown bordered"
+                :toggle-class="{'ps-dropdown__placeholder' : true}"
+                menu-class="ps-dropdown"
+                size="sm"
+              >
+                <b-dropdown-item-button
+                  button-class="text-dark"
+                >
+                  Not available
+                </b-dropdown-item-button>
+                <b-dropdown-form
+                  v-for="option in options"
+                  :key="option.label"
+                >
+                  <b-form-checkbox
+                    class="ps_gs-checkbox"
+                  >
+                    {{ option.label }}
+                  </b-form-checkbox>
+                </b-dropdown-form>
+              </b-dropdown>
+            </b-form-group>
+          </b-col>
+        </b-form-row>
+      </b-form-group>
+    </b-form>
     <product-feed-settings-footer />
   </div>
 </template>
@@ -27,7 +82,62 @@ export default {
     disableContinue() {
       return false;
     },
+    attributes(){
+      return [
+        {
+          category: 'Commons',
+          legend: null,
+          fields: [
+            {
+              label: 'Description',
+              tooltip: true,
+              required: true,
+              default: 'bbb',
+            },
+            {
+              label: 'GTIN (Global Trade Item Number)',
+              tooltip: true,
+              required: true,
+              default: 'bbb',
+            },
+          ],
+        },
+        {
+          category: 'Apparel and Accessories',
+          legend: 'These fields are mandatory if you sell Apparel and Accessories:',
+          fields: [
+            {
+              label: 'Age Group',
+              tooltip: true,
+              required: true,
+              default: 'bbb',
+            },
+          ],
+        },
+        {
+          category: 'Home or Kitchen appliances',
+          legend: 'This field is mandatory if you sell Home or Kitchen appliances:',
+          fields: [
+            {
+              label: 'Energy class',
+              tooltip: true,
+              required: true,
+              default: 'bbb',
+            },
+          ],
+        },
+      ]
+    },
+    options(){
+      return [
+        {
+          label: 'tartiflette au fromage',
+        },
+      ]
+    },
+    /*
     sellApparel: {
+
       get() {
         // TODO
         // To clean in store
@@ -41,6 +151,8 @@ export default {
         );
       },
     },
+    */
+    /*
     sellRefurbished: {
       get() {
         // TODO
@@ -55,6 +167,7 @@ export default {
         );
       },
     },
+    */
   },
   methods: {
     nextStep() {
