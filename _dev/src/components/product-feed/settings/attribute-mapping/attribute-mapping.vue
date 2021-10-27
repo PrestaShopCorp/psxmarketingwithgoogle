c5814782c7462428d748e0942a60005bc6b18f12
Amaury Hanser <amaury.hanser@gmail.com>
2021-10-27 18:01:54 +0200

    Updates attribute mapping markup

--- _dev/src/components/product-feed/settings/attribute-mapping/attribute-mapping.vue
+++ _dev/src/components/product-feed/settings/attribute-mapping/attribute-mapping.vue
@@ -10,31 +10,32 @@
         Google requires filling in a specific list of attributes depending on which product categories you sell. Map these attributes to your custom product fields to ensure your products are validated.
       </p>
       <b-form-group
-        v-for="(category, index) in attributes"
-        :key="category"
+        v-for="(group, index) in attributes"
+        :key="group.category"
         :class="{'border-bottom': index !== attributes.length - 1}"
         class="mb-3 pb-2"
       >
         <VueShowdown
-          v-if="category.legend"
-          :markdown="category.legend"
+          v-if="group.legend"
+          :markdown="group.legend"
           tag="legend"
           class="bg-transparent border-0 ps_gs-fz-14 font-weight-normal mb-3"
           :extensions="['no-p-tag']"
         />
         <b-form-row>
           <b-col
-            v-for="field in category.fields"
+            v-for="field in group.fields"
             :key="field.label"
             :cols="12"
             :md="6"
           >
             <b-form-group
               :label="field.label"
-              :label-for="field.label"
+              :label-for="field.label | slugify"
               label-class="font-weight-600"
             >
               <b-dropdown
+                :id="field.label | slugify"
                 :text="field.default"
                 variant=" "
                 class="maxw-sm-250 ps-dropdown psxmarketingwithgoogle-dropdown bordered"
@@ -43,18 +44,25 @@
                 size="sm"
               >
                 <b-dropdown-item-button
-                  button-class="text-dark"
+                  button-class="rounded-0 text-dark"
                 >
-                  Not available
+                  <span
+                    class="px-2"
+                  >
+                    Not available
+                  </span>
                 </b-dropdown-item-button>
                 <b-dropdown-form
                   v-for="option in options"
-                  :key="option.label"
+                  :key="option.label | slugify"
+                  form-class="dropdown-item dropdown-form-with-checkbox text-dark"
                 >
                   <b-form-checkbox
                     class="ps_gs-checkbox"
                   >
-                    {{ option.label }}
+                    <span class="line-height-15">
+                      "{{ option.label }}"
+                    </span>
                   </b-form-checkbox>
                 </b-dropdown-form>
               </b-dropdown>
@@ -62,6 +70,12 @@
           </b-col>
         </b-form-row>
       </b-form-group>
+      <p class="ps_gs-fz-12 text-muted">
+        <a href="" target="_blank">Learn more about requirements for the Shopping product data specification</a>
+      </p>
+      <p class="ps_gs-fz-12 text-muted">
+        Describe your product data using attributes. Additionally to required minimum for standard free listings, you can add additional attributes required for enhanced listing. General rule is: the more attributes the better.
+      </p>
     </b-form>
     <product-feed-settings-footer />
   </div>
@@ -100,6 +114,18 @@ export default {
               required: true,
               default: 'bbb',
             },
+            {
+              label: 'MPN (Manufacturer Part Number)',
+              tooltip: true,
+              required: true,
+              default: 'mpn',
+            },
+            {
+              label: 'Brand',
+              tooltip: true,
+              required: true,
+              default: 'brand',
+            },
           ],
         },
         {
@@ -131,7 +157,28 @@ export default {
     options(){
       return [
         {
-          label: 'tartiflette au fromage',
+          label: 'Size',
+          value: 'size',
+        },
+        {
+          label: 'ISBN',
+          value: 'isbn',
+        },
+        {
+          label: 'Long description',
+          value: 'longDescription',
+        },
+        {
+          label: 'Fabric color',
+          value: 'fabricColor',
+        },
+        {
+          label: 'Shoe size',
+          value: 'shoeSize',
+        },
+        {
+          label: 'Womens/Mens',
+          value: 'womensMens',
         },
       ]
     },
