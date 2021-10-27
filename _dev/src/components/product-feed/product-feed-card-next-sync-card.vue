185c4454dc083ad155884b1535113676f3be7d5b
Amaury Hanser <amaury.hanser@gmail.com>
2021-10-27 09:53:01 +0200

    Updates style

--- _dev/src/components/product-feed/product-feed-card-next-sync-card.vue
+++ _dev/src/components/product-feed/product-feed-card-next-sync-card.vue
@@ -2,9 +2,9 @@
   <b-col
     cols
     class="ps_gs-productfeed-report-card ps_gs-productfeed-report-card--33
-      bg-violet_extralight border-0"
+      bg-violet_extralight border-0 d-flex"
   >
-    <div class="p-3">
+    <div class="p-3 d-flex flex-column">
       <div class="d-flex align-items-center font-weight-600 ps_gs-fz-13">
         <i
           class="material-icons ps_gs-fz-20 mr-2"
@@ -15,7 +15,7 @@
         <span>{{ title }}</span>
       </div>
       <p
-        class="font-weight-600 ps_gs-fz-16 mt-1 mb-0 text-first-letter-uppercase"
+        class="font-weight-600 ps_gs-fz-16 mt-1 mb-0 text-first-letter-uppercase mt-auto"
       >
         {{ description }}
       </p>
