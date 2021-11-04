deca254e2ff78ed5b6de88c1c4a27a322450fdcb
Thomas Nabord <thomas.nabord@prestashop.com>
2021-11-04 16:20:54 +0100

    Implement getter of PHP carriers

---
+++ _dev/src/providers/shipping-settings-provider.ts
@@ -0,0 +1,40 @@
+export enum ShopShippingCollectionType {
+    CARRIERS = 'carriers',
+    CARRIER_DETAILS = 'carrier_details',
+    CARRIER_TAXES = 'carrier_taxes',
+}
+
+export type ShopShippingInterface = {
+    collection: ShopShippingCollectionType,
+    id: string,
+    properties: {
+        // eslint-disable-next-line camelcase
+        id_carrier: string,
+        name: string,
+        active: boolean,
+        deleted: boolean,
+        delay: string,
+        // eslint-disable-next-line camelcase
+        country_ids: string, // i.e "FR,IT,US"
+    }
+}
+
+export type Carrier = {
+    carrierId: string,
+    country: string
+    name: string,
+    delay: string,
+}
+
+export function getEnabledCarriers(source: ShopShippingInterface[]): Carrier[] {
+  return source.filter((carrier) => carrier.collection === ShopShippingCollectionType.CARRIERS
+        && carrier.properties.active === true
+        && carrier.properties.deleted === false,
+  ).flatMap((carrier) => carrier.properties.country_ids.split(',').map((country) => ({
+    carrierId: carrier.properties.id_carrier,
+    country,
+    name: carrier.properties.name,
+    delay: carrier.properties.delay,
+  })),
+  );
+}
