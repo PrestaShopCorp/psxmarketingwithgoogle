2d69adf097461cb2859d63df412bf5dc38c79ae0
Elise Griset <elise.griset@prestashop.com>
2021-11-05 11:27:22 +0100

    modify params

--- _dev/src/main.ts
+++ _dev/src/main.ts
@@ -49,6 +49,8 @@ new Vue({
           language: this.$i18n.locale,
           version_ps: this.$store.state.app.psVersion,
           ggl_module_version: this.$store.state.app.psxMktgWithGoogleModuleVersion,
+          api_endpoint: this.$store.state.app.psxMktgWithGoogleApiUrl,
+
         });
       }
     },
