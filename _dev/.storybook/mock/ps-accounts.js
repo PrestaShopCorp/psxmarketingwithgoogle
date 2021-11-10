fb882f19f7e6a54b0d1bfb57b239bb1c0c814f98
Amaury Hanser <amaury.hanser@prestashop.com>
2021-11-10 11:32:45 +0100

    Bump version of ps account vue component

--- _dev/.storybook/mock/ps-accounts.js
+++ _dev/.storybook/mock/ps-accounts.js
@@ -1,6 +1,7 @@
 export const contextPsAccountsNotConnected = {
     psxName: "psxmarketingwithgoogle",
     psIs17: true,
+    accountsUiUrl:"https://accounts.distribution.prestashop.net/en",
     psAccountsIsInstalled: true,
     psAccountsInstallLink: null,
     psAccountsIsEnabled: true,
@@ -21,7 +22,7 @@ export const contextPsAccountsNotConnected = {
         url: "https://localhost",
     },
     isShopContext: true,
-    shops: [],
+    shops: [{"id":"1","name":"Default","shops":[{"id":"1","name":"PrestaShop","domain":"placeholder.ngrok.io","domainSsl":"placeholder.ngrok.io","physicalUri":"/","uuid":"d232f98c-65c2-4adc-9352-da5e949531c1","publicKey":"-----BEGIN RSA PUBLIC KEY-----\r\nMIGJAoGBALbXq5oLm/GzCcPb6LSUjgoOue/NHf+XJckP3vlplocTnSW15yKWEAQ0\r\nJPOFHWOhEW9dHxrdv2XUw1vyHhIK3KwJMQnbdzOV0Y0oDPnjIsm3TNhkvHIhq2F2\r\nlPgCW1T1LvLUc+JD2PEMw21W2J5LLDnF/vdnLkqdMMfSmR+34OmDAgMBAAE=\r\n-----END RSA PUBLIC KEY-----","employeeId":1,"url":"https://placeholder.ngrok.io/admin-dev/index.php?controller=AdminModules&configure=psxmarketingwithgoogle&setShopContext=s-1&token=ba2e131a1d891745a4e5389b890fc105","isLinkedV4":false}],"multishop":false,"moduleName":"psxmarketingwithgoogle","psVersion":"1.7.7.2"}],
     superAdminEmail: "some@email.com",
     ssoResendVerificationEmail: "https:\/\/auth.prestashop.com\/account\/send-verification-email",
     manageAccountLink: "https:\/\/auth.prestashop.com\/login",
