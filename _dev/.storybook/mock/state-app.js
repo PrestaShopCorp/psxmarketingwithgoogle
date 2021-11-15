6b34bc5173982cb9e097ef683cdef6e5f19592fa
Amaury Hanser <amaury.hanser@prestashop.com>
2021-11-15 11:28:32 +0100

    Adds CarriersUrl in state-app mock

--- _dev/.storybook/mock/state-app.js
+++ _dev/.storybook/mock/state-app.js
@@ -1,27 +1,28 @@
 export const initialStateApp = {
-    psxMktgWithGoogleApiUrl: '',
-    psxMktgWithGoogleShopUrl: 'https://my-shop.com',
-    psxMktgWithGoogleAdminUrl: 'https://my-shop.com/admin-dev',
-    psxMktgWithGoogleAdminAjaxUrl: '/',
-    shopIdPsAccounts: '',
-    tokenPsAccounts: '',
-    psxMtgWithGoogleDefaultShopCountry: null,
-    isCountryMemberOfEuropeanUnion: false,
-    targetCountries: ['FR'],
-    psxMktgWithGoogleShopCurrency: {
-      isoCode: 'EUR',
-    },
-    psxMktgWithGoogleDocumentAndFaq: {
-      faq: {},
-      doc: '',
-      contactUs: '',
-    },
-    psVersion: '',
-    psxMktgWithGoogleModuleVersion: '',
-    psxMktgWithGoogleMaintenanceSettingsUrl: '',
-    psxMktgWithGoogleStoreSettingsUrl: '',
-    shopIsOnMaintenanceMode: false,
-    psxMktgWithGoogleProductDetailUrl: 'https://my-shop.com/admin-dev/index.php/sell/catalog/products/1?token=blabla'
+  psxMktgWithGoogleApiUrl: '',
+  psxMktgWithGoogleShopUrl: 'https://my-shop.com',
+  psxMktgWithGoogleAdminUrl: 'https://my-shop.com/admin-dev',
+  psxMktgWithGoogleAdminAjaxUrl: '/',
+  shopIdPsAccounts: '',
+  tokenPsAccounts: '',
+  psxMtgWithGoogleDefaultShopCountry: null,
+  isCountryMemberOfEuropeanUnion: false,
+  targetCountries: ['FR'],
+  psxMktgWithGoogleShopCurrency: {
+    isoCode: 'EUR',
+  },
+  psxMktgWithGoogleDocumentAndFaq: {
+    faq: {},
+    doc: '',
+    contactUs: '',
+  },
+  psVersion: '',
+  psxMktgWithGoogleModuleVersion: '',
+  psxMktgWithGoogleMaintenanceSettingsUrl: '',
+  psxMktgWithGoogleStoreSettingsUrl: '',
+  shopIsOnMaintenanceMode: false,
+  psxMktgWithGoogleProductDetailUrl: 'https://my-shop.com/admin-dev/index.php/sell/catalog/products/1?token=blabla',
+  psxMktgWithGoogleCarriersUrl: '/',
 };
 
 export const stateWithMaintenanceModeOn = {
