9bb5eda4602a991b575dfe94757836e649bbeece
Amaury Hanser <amaury.hanser@prestashop.com>
2021-11-16 15:21:42 +0100

    Updates stepper wording

--- _dev/src/components/product-feed/product-feed-settings.vue
+++ _dev/src/components/product-feed/product-feed-settings.vue
@@ -77,10 +77,10 @@ export default {
     return {
       steps: [
         {
-          title: this.$i18n.t('productFeedSettings.steps.shippingSettings'),
+          title: this.$i18n.t('productFeedSettings.steps.targetCountry'),
         },
         {
-          title: this.$i18n.t('productFeedSettings.steps.syncRules'),
+          title: this.$i18n.t('productFeedSettings.steps.shippingSettings'),
         },
         {
           title: this.$i18n.t('productFeedSettings.steps.attributeMapping'),
