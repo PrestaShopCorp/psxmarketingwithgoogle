304dbbbf7331db8f5dcb033a1aaba16fb728081d
Thomas Nabord <thomas.nabord@prestashop.com>
2021-11-19 17:06:25 +0000

    Add mock of availableFilter for Storybook - Story Select filters

--- _dev/stories/smart-shopping-campaign-creation-filter-popin.stories.ts
+++ _dev/stories/smart-shopping-campaign-creation-filter-popin.stories.ts
@@ -1,5 +1,6 @@
-import SmartShoppingCampaignCreationFilterPopin from '../src/components/smart-shopping-campaign-creation/smart-shopping-campaign-creation-filter-popin.vue'
+import SmartShoppingCampaignCreationFilterPopin from '../src/components/smart-shopping-campaign-creation/smart-shopping-campaign-creation-filter-popin.vue';
 import {initialStateApp} from '../.storybook/mock/state-app';
+import {availableFilters} from '../.storybook/mock/smart-shopping-campaigns';
 
 export default {
   title: 'Smart Shopping Campaign/Popins/Select Filters',
@@ -16,10 +17,11 @@ const Template = (args, { argTypes }) => ({
   `,
   beforeMount(this: any) {
     this.$store.state.app = Object.assign({}, initialStateApp);
-  }
+  },
 });
 
 export const SelectFilters:any = Template.bind({});
 SelectFilters.args = {
   visible: true,
+  availableFilters,
 };
