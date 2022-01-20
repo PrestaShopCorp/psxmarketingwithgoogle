9b7216a4b6537f4876bef2ce412aff126545ef84
PoulainMaxime <maxime.poulain@prestashop.com>
2022-01-20 11:52:18 +0100

    put CDN on allowUrls for only trigger module errors

--- _dev/src/utils/Sentry.ts
+++ _dev/src/utils/Sentry.ts
@@ -3,25 +3,28 @@ import * as Sentry from '@sentry/vue';
 import store from '@/store';
 
 // @ts-ignore
-if (store.state.app.psxMktgWithGoogleOnProductionEnvironment) {
-  Sentry.init({
-    Vue,
-    // @ts-ignore
-    dsn: global.psxMktgWithGoogleDsnSentry,
-    tracesSampleRate: 1.0,
-    logErrors: true,
-    initialScope: {
-      tags: {
-        // @ts-ignore
-        'prestashop-version': store.state.app.psVersion,
-        // @ts-ignore
-        'module-version': store.state.app.psxMktgWithGoogleModuleVersion,
-      },
-      user: {
-        id: window.shopIdPsAccounts ? window.shopIdPsAccounts.toString() : 'unknown',
-      },
+// if (store.state.app.psxMktgWithGoogleOnProductionEnvironment) {
+Sentry.init({
+  Vue,
+  // @ts-ignore
+  dsn: global.psxMktgWithGoogleDsnSentry,
+  allowUrls: [
+    process.env.VUE_APP_CDN_URL || '',
+  ],
+  tracesSampleRate: 1.0,
+  logErrors: true,
+  initialScope: {
+    tags: {
+      // @ts-ignore
+      'prestashop-version': store.state.app.psVersion,
+      // @ts-ignore
+      'module-version': store.state.app.psxMktgWithGoogleModuleVersion,
     },
-    // @ts-ignore
-    release: `v${store.state.app.psxMktgWithGoogleModuleVersion}`,
-  });
-}
+    user: {
+      id: window.shopIdPsAccounts ? window.shopIdPsAccounts.toString() : 'unknown',
+    },
+  },
+  // @ts-ignore
+  release: `v${store.state.app.psxMktgWithGoogleModuleVersion}`,
+});
+// }
