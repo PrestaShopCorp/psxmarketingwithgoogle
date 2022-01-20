819c0c0d4ae77faacd9be7f13add720915c341bc
Amaury Hanser <amaury.hanser@prestashop.com>
2022-01-20 09:41:35 +0100

    Test ga workflow

--- _dev/src/App.vue
+++ _dev/src/App.vue
@@ -16,8 +16,8 @@
     <template v-else>
       <div class="ps_gs-sticky-head">
         <Menu>
-        <!-- eslint-disable-next-line -->
-        <!-- We display the tab if user has remarketing tag in the module OR already set elsewhere -->
+          <!-- eslint-disable-next-line -->
+          <!-- We display the tab if user has remarketing tag in the module OR already set elsewhere -->
           <template v-if="reportingTabVisible">
             <MenuItem
               @click.native="throwSegmentEvent"
