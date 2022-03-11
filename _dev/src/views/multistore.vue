a4590ea415bcd6a9fffedf546d49453730b63c83
Elise Griset <elise.griset@prestashop.com>
2022-03-11 11:04:15 +0100

    Fix-router-redirection-landingpage

--- _dev/src/views/multistore.vue
+++ _dev/src/views/multistore.vue
@@ -1,25 +1,19 @@
 <template>
   <div class="pt-2">
-    <template v-if="!psAccountsContext.isShopContext && shops.length">
-      <MultiStoreSelector
-        v-if="!psAccountsContext.isShopContext && shops.length"
-        :shops="shops"
-        @shop-selected="onShopSelected($event)"
-      />
-    </template>
-    <landing-page v-else />
+    <MultiStoreSelector
+      :shops="shops"
+      @shop-selected="onShopSelected($event)"
+    />
   </div>
 </template>
 
 <script>
 import MultiStoreSelector from '@/components/multistore/multi-store-selector.vue';
-import LandingPage from './landing-page.vue';
 
 export default {
   name: 'Multistore',
   components: {
     MultiStoreSelector,
-    LandingPage,
   },
   data() {
     return {
@@ -32,10 +26,6 @@ export default {
     },
   },
   computed: {
-
-    psAccountsContext() {
-      return this.$store.getters['accounts/GET_PS_ACCOUNTS_CONTEXT'];
-    },
     shops() {
       return this.$store.getters['accounts/GET_PS_ACCOUNTS_CONTEXT_SHOPS'];
     },
