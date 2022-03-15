d25fe6d0a7a3682282bc12b94607060392ce1951
Elise Griset <elise.griset@prestashop.com>
2022-03-15 18:57:13 +0100

    Add test

--- _dev/src/views/landing-page.spec.ts
+++ _dev/src/views/landing-page.spec.ts
@@ -3,16 +3,28 @@
  */
 
 // Import this file first to init mock on window
-import {shallowMount} from '@vue/test-utils';
-import config from '@/../tests/init';
+import { shallowMount } from "@vue/test-utils";
+import Vuex from "vuex";
+import config, { cloneStore } from "@/../tests/init";
+import cloneDeep from 'lodash.clonedeep';
+import {initialStateApp} from '../../.storybook/mock/state-app';
 
-import LandingPage from '@/views/landing-page.vue';
+import LandingPage from "@/views/landing-page.vue";
 
-describe('landing-page.vue', () => {
-  it('shows all the onboarding details', () => {
+describe("landing-page.vue", () => {
+  let store;
+  beforeEach(() => {
+    store = cloneStore();
+    store.modules.app.state= {
+      ...cloneDeep(initialStateApp),
+     }
+  });
+
+  it("shows all the onboarding details", () => {
     const wrapper = shallowMount(LandingPage, {
       ...config,
+      store: new Vuex.Store(store),
     });
-    expect(wrapper.find('.ps_gs-landingpage').isVisible()).toBe(true);
+    expect(wrapper.find(".ps_gs-landingpage").isVisible()).toBe(true);
   });
 });
