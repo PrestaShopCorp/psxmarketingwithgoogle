c1657525bde151c94e54b924bc79fc9d2fe8b525
Elise Griset <elise.griset@prestashop.com>
2022-05-27 16:44:16 +0200

    remove wrong code

--- _dev/tests/init.ts
+++ _dev/tests/init.ts
@@ -77,9 +77,6 @@ config.mocks.$te = (key) => {
 config.mocks.$segment = {
   track: () => null,
 };
-config.mocks.window = {
-  addEventListener: () => null,
-};
 
 config.mocks.$i18n = {
   t: config.mocks.$t,
