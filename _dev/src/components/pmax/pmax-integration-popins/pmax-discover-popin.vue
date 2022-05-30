53b8e3f439affc1c9ce23e5f35fe2f0811f636da
PoulainMaxime <maxime.poulain@prestashop.com>
2022-05-30 09:19:53 +0200

    create pmax popin

---
+++ _dev/src/components/pmax/pmax-integration-popins/pmax-discover-popin.vue
@@ -0,0 +1,94 @@
+<template>
+  <ps-modal
+    id="PmaxDiscoverPopin"
+    ref="modal"
+    :title="content.title"
+    v-bind="$attrs"
+  >
+    <div class="square" style="border:1px solid grey; width:50px;height:50px;">
+    </div>
+
+    <VueShowdown
+      class="my-1 mt-4"
+      :markdown="content.body"
+    />
+
+    <div class="mt-4">
+      <span class="material-icons">
+        link
+      </span>
+      <a
+        class="external_link-no_icon text-muted"
+        :href="$options.googleUrl.pmaxReleaseLink"
+        target="_blank"
+      >
+        {{ $t('modal.PmaxTextLink') }}
+      </a>
+    </div>
+
+    <template slot="modal-cancel">
+      {{ $t('cta.cancel') }}
+    </template>
+    <template slot="modal-ok">
+      {{ this.type === PmaxPopinType.PMAX_RELEASED ? $t('cta.createPmax') : $t('cta.done') }}
+    </template>
+  </ps-modal>
+</template>
+<script>
+import PsModal from '../../commons/ps-modal';
+import { PmaxPopinType } from '../../../enums/pmax/pmax-popin';
+import googleUrl from '../../../assets/json/googleUrl.json';
+
+export default {
+name: 'PmaxDiscoverPopin',
+  components: {
+    PsModal,
+  },
+  data() {
+    return {
+      PmaxPopinType,
+      content: {
+        title: '',
+        body: '',
+        img: '',
+      },
+    };
+  },
+  props: {
+    type: {
+      type: String,
+      required: true,
+      validator(value) {
+        if (value === null) {
+          return true;
+        }
+        return Object.values(PmaxPopinType).includes(value);
+      },
+    },
+  },
+  mounted() {
+    switch (this.type) {
+      case PmaxPopinType.COMING_SOON:
+        this.content.title = this.$t('modal.upgradeSCCToPmaxTitle');
+        this.content.body = this.$t('modal.upgradeSCCToPmaxText', [
+          new Date().toLocaleDateString()
+        ]);
+        this.content.img = ''
+        break;
+      case PmaxPopinType.PMAX_RELEASED:
+        this.content.title = this.$t('modal.PMaxLiveTitle');
+        this.content.body = this.$t('modal.PMaxLiveText');
+        this.content.img = ''
+        break;
+      case PmaxPopinType.SSC_DEPRECEATED:
+        this.content.title = this.$t('modal.PMaxMigrationTitle');
+        this.content.body = this.$t('modal.PMaxMigrationText');
+        this.content.img = ''
+        break;
+      default:
+        break;
+    }
+  },
+  googleUrl,
+};
+</script>
---
+++ _dev/src/enums/pmax/pmax-popin.ts
@@ -0,0 +1,5 @@
+export enum PmaxPopinType {
+  COMING_SOON = 'coming-soon',
+  PMAX_RELEASED = 'pmax-released',
+  SSC_DEPRECEATED = 'ssc-deprecated',
+}
