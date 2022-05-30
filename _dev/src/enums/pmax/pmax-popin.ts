00f4c35f551b03e9c9620d261b8be3bf58313b11
PoulainMaxime <maxime.poulain@prestashop.com>
2022-05-30 09:28:27 +0200

    resolve ci

--- _dev/src/enums/pmax/pmax-popin.ts
+++ _dev/src/enums/pmax/pmax-popin.ts
@@ -1,5 +1,7 @@
-export enum PmaxPopinType {
+enum PmaxPopinType {
   COMING_SOON = 'coming-soon',
   PMAX_RELEASED = 'pmax-released',
   SSC_DEPRECEATED = 'ssc-deprecated',
 }
+
+export default PmaxPopinType;
