8e35f864b472751bac3573def8292029496aeb60
Elise Griset <elise.griset@prestashop.com>
2022-06-02 17:00:44 +0200

    remove unused variable

--- _dev/src/store/modules/smart-shopping-campaigns/state.ts
+++ _dev/src/store/modules/smart-shopping-campaigns/state.ts
@@ -21,7 +21,7 @@ import dayjs from 'dayjs';
 import KpiType from '@/enums/reporting/KpiType';
 import QueryOrderDirection from '@/enums/reporting/QueryOrderDirection';
 import ReportingPeriod from '@/enums/reporting/ReportingPeriod';
-import CampaignStatus, {CampaignStatusToggle, CampaignTypes} from '@/enums/reporting/CampaignStatus';
+import CampaignStatus, {CampaignStatusToggle} from '@/enums/reporting/CampaignStatus';
 
 export interface State {
   campaigns: CampaignObject[];
