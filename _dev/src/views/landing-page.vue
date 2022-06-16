66071b79fee6ea4e3ec8e9fb3d17e5ea92ad511d
Thomas N <Quetzacoalt91@users.noreply.github.com>
2022-06-16 16:19:10 +0200

    Merge pull request #1091 from PoulainMaxime/imp/pmax-wording
    change wording for landing page

--- _dev/src/views/landing-page.vue
+++ _dev/src/views/landing-page.vue
@@ -1,43 +1,51 @@
 <template>
-  <b-card no-body>
-    <div class="ps_gs-landingpage">
-      <LandingPageHeader @hideLandingPage="hideLandingPage('header')" />
-      <hr class="my-4">
-      <LandingPageContent
-        :svg-image="false"
-        content-image="Merchant-Center-img.png"
-        :content-title="$t('landingPage.content.content1.title')"
-        :content-text="$t('landingPage.content.content1.text')"
-      />
-      <LandingPageContent
-        :svg-image="false"
-        content-image="Google-Shopping-Tab-img.png"
-        :content-title="$t('landingPage.content.content2.title')"
-        :content-text="$t('landingPage.content.content2.text')"
-        :content-footer="$t('landingPage.content.content2.footer', [
-          $options.googleUrl.createNewMerchantCenter,
-          $options.googleUrl.eligibilityRequirements
-        ])"
-      />
-      <LandingPageContent
-        :svg-image="false"
-        content-image="Google-Commercial-img.png"
-        :content-title="$t('landingPage.content.content3.title')"
-        :content-text="$t('landingPage.content.content3.text')"
-      />
-      <LandingPageContent
-        :svg-image="true"
-        :content-image="pngBanner"
-        :content-title="$t('banner.titleAdsbanner')"
-        :content-text="$t('banner.textAdsBanner', [priceForAds])"
-        :content-footer="$t('banner.legendLong', [
-          $options.googleUrl.googleAdsTermsAndCondition
-        ])"
-      />
-      <hr class="my-4">
-      <LandingPageFooter @hideLandingPage="hideLandingPage('footer')" />
-    </div>
-  </b-card>
+  <div>
+    <b-card no-body class="mb-4">
+      <div class="ps_gs-landingpage">
+        <LandingPageHeader @hideLandingPage="hideLandingPage('header')" />
+        <hr class="my-4">
+        <LandingPageContent
+          :svg-image="false"
+          content-image="Merchant-Center-img.png"
+          :content-title="$t('landingPage.content.content1.title')"
+          :content-text="$t('landingPage.content.content1.text')"
+        />
+        <LandingPageContent
+          :svg-image="false"
+          content-image="Google-Shopping-Tab-img.png"
+          :content-title="$t('landingPage.content.content2.title')"
+          :content-text="$t('landingPage.content.content2.text')"
+          :content-footer="$t('landingPage.content.content2.footer', [
+            $options.googleUrl.createNewMerchantCenter,
+            $options.googleUrl.eligibilityRequirements
+          ])"
+        />
+        <LandingPageContent
+          :svg-image="false"
+          content-image="Google-Commercial-img.png"
+          :content-title="$t('landingPage.content.content3.title')"
+          :content-text="$t('landingPage.content.content3.text')"
+          :content-footer="$t('landingPage.content.content3.footer', [
+            $options.googleUrl.createNewMerchantCenter,
+          ])"
+        />
+        <LandingPageContent
+          :svg-image="true"
+          :content-image="pngBanner"
+          :content-title="$t('banner.titleAdsbanner')"
+          :content-text="$t('banner.textAdsBanner', [priceForAds])"
+          :content-footer="$t('banner.legendLong', [
+            $options.googleUrl.googleAdsTermsAndCondition
+          ])"
+        />
+        <hr class="my-4">
+        <LandingPageFooter @hideLandingPage="hideLandingPage('footer')" />
+      </div>
+    </b-card>
+    <p class="ps_gs-landingpage-content__muted text-muted text-center">
+      {{ $t('landingPage.footer.legal') }}
+    </p>
+  </div>
 </template>
 
 <script>
