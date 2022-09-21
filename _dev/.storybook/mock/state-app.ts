326ae0bf80b9ef72a97cee2f1706acbe77f6d82d
Thomas Nabord <thomas.nabord@prestashop.com>
2022-09-21 12:41:06 +0100

    Fix partial state

--- _dev/.storybook/mock/state-app.ts
+++ _dev/.storybook/mock/state-app.ts
@@ -1,7 +1,7 @@
-import {State} from "@/store/modules/app/state";
+import {State, state} from "@/store/modules/app/state";
 
-export const initialStateApp: Partial<State> = {
-  psxMktgWithGoogleApiUrl: '',
+export const initialStateApp: State = {
+  ...state,
   psxMktgWithGoogleShopUrl: 'https://my-shop.com',
   psxMktgWithGoogleAdminUrl: 'https://my-shop.com/admin-dev',
   psxMktgWithGoogleAdminAjaxUrl: '/',
@@ -10,21 +10,10 @@ export const initialStateApp: Partial<State> = {
   psxMktgWithGoogleShopCurrency: {
     isoCode: 'EUR',
   },
-  psxMktgWithGoogleDocumentAndFaq: {
-    faq: {},
-    doc: '',
-    contactUs: '',
-  },
-  psVersion: '',
-  psxMktgWithGoogleModuleVersion: '',
   psxMktgWithGoogleModuleVersionNeeded: '1.10.4',
   eventbusVersionNeeded: '1.6.5',
-  psxMktgWithGoogleMaintenanceSettingsUrl: '',
-  psxMktgWithGoogleStoreSettingsUrl: '',
   shopIsOnMaintenanceMode: false,
   psxMktgWithGoogleProductDetailUrl: 'https://my-shop.com/admin-dev/index.php/sell/catalog/products/1?token=blabla',
-  psxMktgWithGoogleCarriersUrl: '/',
-  psxMktgWithGoogleAttributesUrl: '/',
 };
 
 export const stateWithMaintenanceModeOn = {
