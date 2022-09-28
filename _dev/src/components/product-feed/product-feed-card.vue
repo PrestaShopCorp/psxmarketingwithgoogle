fb3e64ad1fd1fb585b4908ffd1ac4a2355a501c8
PoulainMaxime <maxime.poulain@prestashop.com>
2022-09-28 17:32:07 +0200

    Restore wording when merchant start productFeed on stepper

--- _dev/src/components/product-feed/product-feed-card.vue
+++ _dev/src/components/product-feed/product-feed-card.vue
@@ -69,7 +69,7 @@
               @click="startConfiguration"
               :disabled="isErrorApi"
             >
-              {{ $t("cta.configureAndExportProductFeed") }}
+              {{ getActiveStep > 1 ? $t("cta.continueProductFeed") : $t("cta.configureAndExportProductFeed") }}
             </b-button>
           </div>
         </div>
