ed2df26ce78967513b22cf4056e82c29fc863e84
PoulainMaxime <maxime.poulain@prestashop.com>
2023-03-02 17:32:57 +0100

    Change constructor call

--- _dev/packages/mktg-with-google-common/api/HttpClientError.ts
+++ _dev/packages/mktg-with-google-common/api/HttpClientError.ts
@@ -1,7 +1,7 @@
 export default class HttpClientError extends Error implements Error {
   constructor(message: string, public code: number) {
     super(message);
-    this.name = this.constructor().name;
+    this.name = this.constructor.name;
     this.message = message;
     this.code = code;
   }
