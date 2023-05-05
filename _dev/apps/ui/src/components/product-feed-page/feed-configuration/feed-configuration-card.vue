f47828f8f7bc5f3ac523d2a08bd63b473e073acc
Thomas Nabord <thomas.nabord@prestashop.com>
2023-05-05 12:47:01 +0100

    Update stories to display config used and plug to vuex

--- _dev/apps/ui/src/components/product-feed-page/feed-configuration/feed-configuration-card.vue
+++ _dev/apps/ui/src/components/product-feed-page/feed-configuration/feed-configuration-card.vue
@@ -1,49 +1,65 @@
 <template>
-  <div
-    class="d-flex flex-column border border-600-20 rounded mb-2 px-3 py-2 d-flex bg-prestashop-bg"
-  >
-    <div class="mb-3">
-      <span class="font-weight-600 mb-1 mr-2 h3">
-        {{ $t('productFeedPage.dashboardPage.productFeedConfiguration.title') }}
-      </span>
-      <span
-        class="ps_gs-fz-12"
-        v-if="productFeedConfiguration"
+  <section>
+    <div
+      class="d-flex flex-column border border-600-20 rounded mb-2 px-3 py-2 d-flex bg-prestashop-bg"
+    >
+      <b-skeleton-wrapper
+        :loading="loading"
       >
-        {{ lastModificationDate }}, {{ lastModificationTime }}
-      </span>
-    </div>
+        <template #loading>
+          <b-skeleton width="30%" class="mb-3" />
+          <div
+            class="d-flex"
+          >
+            <b-skeleton width="50%" class="mr-3"/>
+            <b-skeleton width="50%" />
+          </div>
+        </template>
 
-    <div class="d-flex justify-content-between">
-      <div class="flex-grow-1">
-        <i class="material-icons ps_gs-fz-20">language</i>
-        <span class="font-weight-600">
-          {{ $tc('productFeedPage.dashboardPage.productFeedConfiguration.targetCountries', localizationListLengths.countries) }}
-        </span>
-        <b-card
-          v-for="country in targetCountriesNames"
-          border-variant="primary"
-          class="mx-1 d-inline-flex ps_gs-productfeed__badge ps_gs-fz-13"
-        >
-          {{ country }}
-        </b-card>
-      </div>
-
-      <div class="flex-grow-1">
-        <i class="material-icons ps_gs-fz-20">translate</i>
-        <span class="font-weight-600">
-          {{ $tc('productFeedPage.dashboardPage.productFeedConfiguration.languages', localizationListLengths.languages) }}
-        </span>
-        <b-card
-          v-for="language in languages"
-          border-variant="primary"
-          class="mx-1 d-inline-flex ps_gs-productfeed__badge ps_gs-fz-13"
-        >
-          {{ language }}
-        </b-card>
-      </div>
+        <div class="mb-3">
+          <span class="font-weight-600 mb-1 mr-2 h3">
+            {{ $t('productFeedPage.dashboardPage.productFeedConfiguration.title') }}
+          </span>
+          <span
+            class="ps_gs-fz-12"
+            v-if="productFeedConfiguration"
+          >
+            {{ lastModificationDate }}, {{ lastModificationTime }}
+          </span>
+        </div>
+  
+        <div class="d-flex justify-content-between">
+          <div class="flex-grow-1">
+            <i class="material-icons ps_gs-fz-20">language</i>
+            <span class="font-weight-600">
+              {{ $tc('productFeedPage.dashboardPage.productFeedConfiguration.targetCountries', localizationListLengths.countries) }}
+            </span>
+            <b-card
+              v-for="country in targetCountriesNames"
+              border-variant="primary"
+              class="mx-1 d-inline-flex ps_gs-productfeed__badge ps_gs-fz-13"
+            >
+              {{ country }}
+            </b-card>
+          </div>
+  
+          <div class="flex-grow-1">
+            <i class="material-icons ps_gs-fz-20">translate</i>
+            <span class="font-weight-600">
+              {{ $tc('productFeedPage.dashboardPage.productFeedConfiguration.languages', localizationListLengths.languages) }}
+            </span>
+            <b-card
+              v-for="language in languages"
+              border-variant="primary"
+              class="mx-1 d-inline-flex ps_gs-productfeed__badge ps_gs-fz-13"
+            >
+              {{ language }}
+            </b-card>
+          </div>
+        </div>
+      </b-skeleton-wrapper>
     </div>
-  </div>
+  </section>
 </template>
 
 <script lang="ts">
@@ -60,6 +76,9 @@ export default defineComponent({
       type: [Object, null] as PropType<ProductFeedConfiguration>,
       required: false,
       default: null,
+    },
+    loading: {
+      type: Boolean,
     },
   },
   computed: {
