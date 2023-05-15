c1dbb72f98d5d4b75235c21fdef8a0d7e362322c
Thomas Nabord <thomas.nabord@prestashop.com>
2023-05-15 17:55:07 +0200

    Fix generation of links to GMC

--- _dev/apps/ui/src/components/merchant-center-account/merchant-center-account-links.ts
+++ _dev/apps/ui/src/components/merchant-center-account/merchant-center-account-links.ts
@@ -1,4 +1,4 @@
-const getMerchantCenterWebsiteUrls = (id: string) => ({
+export const getMerchantCenterWebsiteUrls = (id: string) => ({
   website: `https://merchants.google.com/mc/settings/website?a=${id}`,
   businessInfo: `https://merchants.google.com/mc/merchantprofile/businessinfo?a=${id}`,
   overview: `https://merchants.google.com/mc/overview?a=${id}`,
