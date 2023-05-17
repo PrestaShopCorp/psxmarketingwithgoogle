d0ac0a7a674b619ad5ed5dcb80c02806c7a386cb
Thomas Nabord <thomas.nabord@prestashop.com>
2023-05-17 16:16:31 +0200

    Update titles text for verified & submitted products

--- _dev/apps/ui/src/components/product-feed-page/sync-overview.vue
+++ _dev/apps/ui/src/components/product-feed-page/sync-overview.vue
@@ -41,6 +41,9 @@
           :product-feed-configuration="incrementalSyncContext"
           :loading="loading"
         />
+        <verified-products
+          :loading="loading"
+        />
         <SubmittedProducts
           v-slot="{ productStatuses }"
           :in-need-of-configuration="inNeedOfConfiguration"
@@ -62,6 +65,7 @@ import NotConfiguredCard from '@/components/commons/not-configured-card';
 import FeedConfigurationCard from './feed-configuration/feed-configuration-card.vue';
 import MerchantCenterAccountAlertSuspended from '@/components/merchant-center-account/merchant-center-account-alert-suspended.vue';
 import SubmittedProducts from './submitted-products/submitted-products';
+import VerifiedProducts from './verified-products/verified-products';
 import ProductsStatusCard from './submitted-products/products-status-card';
 import SyncHistory from './sync-history/sync-history';
 import SyncState from './sync-history/sync-state';
@@ -78,6 +82,7 @@ export default defineComponent({
     SubmittedProducts,
     SyncHistory,
     SyncState,
+    VerifiedProducts,
   },
   props: {
     inNeedOfConfiguration: {
