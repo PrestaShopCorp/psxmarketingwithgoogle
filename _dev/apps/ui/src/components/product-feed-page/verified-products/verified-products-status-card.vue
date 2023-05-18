14872fbded3142a381d5c759e13e4ab02cd20952
Thomas Nabord <thomas.nabord@prestashop.com>
2023-05-18 11:45:58 +0200

    Add loading state, link and plug to storybook

--- _dev/apps/ui/src/components/product-feed-page/verified-products/verified-products-status-card.vue
+++ _dev/apps/ui/src/components/product-feed-page/verified-products/verified-products-status-card.vue
@@ -2,41 +2,72 @@
   <div
     class="ps_gs-productfeed__products-status border border-600-20 rounded col mx-1"
   >
-    <div
-      class="d-flex flex-column align-items-center p-2"
+    <b-skeleton-wrapper
+      :loading="loading"
     >
-      <span
-        class="d-flex align-items-center ps_gs-fz-18 font-weight-600 text-center pt-1 pb-2"
+      <template #loading>
+        <div
+          class="d-flex flex-column align-items-center p-2"
+        >
+          <b-skeleton
+            width="70%"
+            class="mt-2 mb-3"
+          />
+          <b-skeleton
+            width="100%"
+          />
+          <b-skeleton
+            width="100%"
+            class="mb-2"
+          />
+          <b-skeleton
+            width="20%"
+            height="2.5em"
+          />
+        </div>
+      </template>
+
+      <div
+        class="d-flex flex-column align-items-center p-2"
       >
         <span
-          class="material-icons-round ps_gs-fz-20 mr-1"
-          :class="`text-${status.variant}`"
+          class="d-flex align-items-center ps_gs-fz-18 font-weight-600 text-center pt-1 pb-2"
         >
-          {{ status.icon }}
+          <span
+            class="material-icons-round ps_gs-fz-20 mr-1"
+            :class="`text-${status.variant}`"
+          >
+            {{ status.icon }}
+          </span>
+          {{ status.title }}
         </span>
-        {{ status.title }}
-      </span>
-      <span
-        class="text-center pb-1"
-      >
-      {{ status.description }}
-      </span>
-      <b-card
-        :border-variant="badgeBorderVariant"
-        :text-variant="badgeTextVariant"
-        :bg-variant="badgeBackgroundVariant"
-        class="ps_gs-productfeed__badge ps_gs-fz-20 mt-auto"
-        body-class="py-0 px-3"
-      >
-        {{ badgeValue }}
-      </b-card>
-    </div>
+        <span
+          class="text-center pb-1"
+        >
+          {{ status.description }}
+        </span>
+        <b-card
+          :border-variant="badgeBorderVariant"
+          :text-variant="badgeTextVariant"
+          :bg-variant="badgeBackgroundVariant"
+          class="ps_gs-productfeed__badge ps_gs-fz-20 mt-auto"
+          body-class="py-0 px-3"
+        >
+          {{ badgeValue }}
+        </b-card>
+        <b-link
+          v-if="status.link"
+          v-bind="status.link"
+          class="stretched-link"
+        />
+      </div>
+    </b-skeleton-wrapper>
   </div>
 </template>
 
 <script lang="ts">
-import { PropType, defineComponent } from 'vue';
-import { StatusCard } from './verified-products';
+import {PropType, defineComponent} from 'vue';
+import {StatusCard} from './verified-products';
 
 export default defineComponent({
   props: {
