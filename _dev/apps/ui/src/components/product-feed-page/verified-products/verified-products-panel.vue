2e447590338b4b66de7872388305afc34b107ff5
Thomas Nabord <thomas.nabord@prestashop.com>
2023-05-18 11:00:17 +0200

    Start integration of verified cards

---
+++ _dev/apps/ui/src/components/product-feed-page/verified-products/verified-products-panel.vue
@@ -0,0 +1,91 @@
+<template>
+  <div>
+    <div class="d-flex align-items-center mb-2">
+      <h2 class="ps_gs-fz-16 font-weight-600">
+        {{ $t('productFeedPage.dashboardPage.productVerification.stepTitle') }}
+      </h2>
+      <b-button
+        id="tooltip-verified-product"
+        class="ml-1 p-0 d-flex"
+        variant="text"
+      >
+        <span class="material-icons-round ps_gs-fz-20 mb-1 ml-0 text-secondary">
+          info_outlined
+        </span>
+      </b-button>
+      <b-tooltip
+        target="tooltip-verified-product"
+        triggers="hover"
+        container="#psxMktgWithGoogleApp"
+      >
+        <VueShowdown
+          :markdown="$t('productFeedPage.dashboardPage.productVerification.stepDetails')"
+        />
+      </b-tooltip>
+    </div>
+    <div class="p-0 container-fluid">
+      <div class="row mx-n1 no-gutters mb-3">
+        <status-card-component
+          v-for="(status, index) in statusCards"
+          :key="index"
+          :status="status"
+          :loading="loading"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import {PropType, defineComponent} from 'vue';
+import {VueShowdown} from 'vue-showdown';
+import StatusCardComponent from './verified-products-status-card.vue';
+import {StatusCard, VerificationStats} from './verified-products';
+
+export default defineComponent({
+  components: {
+    StatusCardComponent,
+    VueShowdown,
+  },
+  props: {
+    verificationsStats: {
+      type: [Object, null] as PropType<VerificationStats>,
+      required: false,
+      default: null,
+    },
+    loading: {
+      type: Boolean,
+      required: true,
+    },
+  },
+  computed: {
+    statusCards(): StatusCard[] {
+      return [{
+          title: this.$t('productFeedPage.dashboardPage.productVerification.reportCards.productsInCatalog'),
+          description: this.$t('productFeedPage.dashboardPage.productVerification.reportCards.productsInCatalogDescription'),
+          value: this.verificationsStats?.productsInCatalog,
+          variant: 'info',
+          icon: 'redeem',
+          reverseColors: false,
+        },
+        {
+          title: this.$t('productFeedPage.dashboardPage.productVerification.reportCards.verified'),
+          description: this.$t('productFeedPage.dashboardPage.productVerification.reportCards.verifiedDescription'),
+          value: this.verificationsStats?.verifiedProducts,
+          variant: 'success',
+          icon: 'send',
+          reverseColors: false,
+        },
+        {
+          title: this.$t('productFeedPage.dashboardPage.productVerification.reportCards.nonCompliant'),
+          description: this.$t('productFeedPage.dashboardPage.productVerification.reportCards.nonCompliantDescription'),
+          value: this.verificationsStats?.nonCompliantProducts,
+          variant: 'danger',
+          icon: 'remove_shopping_cart',
+          reverseColors: !!+this.verificationsStats?.nonCompliantProducts,
+        },
+      ];
+    },
+  },
+});
+</script>
---
+++ _dev/apps/ui/src/components/product-feed-page/verified-products/verified-products-status-card.vue
@@ -0,0 +1,76 @@
+<template>
+  <div
+    class="ps_gs-productfeed__products-status border border-600-20 rounded col mx-1"
+  >
+    <div
+      class="d-flex flex-column align-items-center p-2"
+    >
+      <span
+        class="d-flex align-items-center ps_gs-fz-18 font-weight-600 text-center pt-1 pb-2"
+      >
+        <span
+          class="material-icons-round ps_gs-fz-20 mr-1"
+          :class="`text-${status.variant}`"
+        >
+          {{ status.icon }}
+        </span>
+        {{ status.title }}
+      </span>
+      <span
+        class="text-center pb-1"
+      >
+      {{ status.description }}
+      </span>
+      <b-card
+        :border-variant="badgeBorderVariant"
+        :text-variant="badgeTextVariant"
+        :bg-variant="badgeBackgroundVariant"
+        class="ps_gs-productfeed__badge ps_gs-fz-20 mt-auto"
+        body-class="py-0 px-3"
+      >
+        {{ badgeValue }}
+      </b-card>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { PropType, defineComponent } from 'vue';
+import { StatusCard } from './verified-products';
+
+export default defineComponent({
+  props: {
+    status: {
+      type: Object as PropType<StatusCard>,
+      required: true,
+    },
+    loading: {
+      type: Boolean,
+      required: true,
+    },
+  },
+  computed: {
+    badgeValue(): string {
+      return this.status.value || '--';
+    },
+    badgeBorderVariant(): string|null {
+      if (this.status.reverseColors) {
+        return null;
+      }
+      return this.status.variant;
+    },
+    badgeTextVariant(): string|null {
+      if (this.status.reverseColors) {
+        return 'white';
+      }
+      return this.status.variant;
+    },
+    badgeBackgroundVariant(): string|null {
+      if (this.status.reverseColors) {
+        return this.status.variant;
+      }
+      return 'white';
+    },
+  },
+});
+</script>
