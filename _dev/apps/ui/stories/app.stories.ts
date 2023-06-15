a9ff3466d6dc684502d3a09a456636ac90e96c8f
Thomas Nabord <thomas.nabord@prestashop.com>
2023-06-15 14:48:20 +0100

    Fix Overall story on storybook

--- _dev/apps/ui/stories/app.stories.ts
+++ _dev/apps/ui/stories/app.stories.ts
@@ -1,4 +1,13 @@
 import App from '@/App.vue';
+import cloneDeep from 'lodash.clonedeep';
+import {googleAccountConnected} from '../.storybook/mock/google-account';
+import {conversionTrackingIsTrue} from '../.storybook/mock/campaigns';
+import {campaigns} from '../.storybook/mock/campaigns-list';
+import {contextPsAccountsConnectedAndValidated} from "../.storybook/mock/ps-accounts";
+import {initialStateApp} from "../.storybook/mock/state-app";
+import {merchantCenterAccountConnected} from "../.storybook/mock/merchant-center-account";
+import {adsAccountStatus} from '../.storybook/mock/google-ads';
+import {productFeedStatusSyncSuccess} from '../.storybook/mock/product-feed';
 
 export default {
   title: 'Whole application/Overview',
@@ -13,6 +22,17 @@ const Template = (args, { argTypes }) => ({
       <App />
     </div>
   `,
+  beforeCreate(this:any) {
+    this.$store.state.app = cloneDeep(initialStateApp);
+    this.$store.state.accounts.contextPsAccounts = cloneDeep(contextPsAccountsConnectedAndValidated);
+    this.$store.state.accounts.googleAccount = cloneDeep(googleAccountConnected);
+    this.$store.state.accounts.googleMerchantAccount = cloneDeep(merchantCenterAccountConnected);
+    this.$store.state.productFeed = cloneDeep(productFeedStatusSyncSuccess);
+    this.$store.state.campaigns = cloneDeep(conversionTrackingIsTrue);
+    this.$store.state.campaigns.campaigns = cloneDeep(campaigns);
+    this.$store.state.campaigns.conversionActions = [{}];
+    this.$store.state.googleAds = cloneDeep(adsAccountStatus);
+  },
 });
 
 export const Overview:any = Template.bind({});
