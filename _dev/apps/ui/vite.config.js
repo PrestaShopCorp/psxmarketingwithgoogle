197dbfcf26bcd73eac3f702f0d6b6e02d6eb7dab
Thomas Nabord <thomas.nabord@prestashop.com>
2023-06-23 12:16:58 +0100

    Make vite build complete

--- _dev/apps/ui/vite.config.js
+++ _dev/apps/ui/vite.config.js
@@ -1,16 +1,30 @@
 import { defineConfig } from 'vite';
 import { createVuePlugin as vue } from "vite-plugin-vue2";
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
+    alias: [
+      {
+        find: "@",
+        replacement: path.resolve(__dirname, "./src"),
+      },
+      {
+          // this is required for the SCSS modules
+          find: /^~(.*)$/,
+          replacement: '$1',
+      },
+    ],
     // TODO: to remove as soon as we made sure that all import have the .vue extension
-    extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue'],
+    extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue', '.scss'],
   },
+  build: {
+    outDir: '../../../views/',
+    assetsDir: '',
+    publicPath: process.env.VITE_ASSETS_URL || '../modules/psxmarketingwithgoogle/views/',
+  }
   // TODO: Move rules used to create build files in the proper folder
   // + remove test attributes in production mode
   // from vue.config.js
