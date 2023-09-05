77b13cd2d0f95b58b1bbc8c4c2e4e8cb88088e2a
Thomas Nabord <thomas.nabord@prestashop.com>
2023-09-05 18:18:08 +0200

    Implement tests on campaign list (Header)

--- _dev/apps/ui/src/components/campaign/campaign-table-list.spec.ts
+++ _dev/apps/ui/src/components/campaign/campaign-table-list.spec.ts
@@ -1,12 +1,14 @@
 import Vuex from 'vuex';
 
 // Import this file first to init mock on window
-import {MountOptions, mount} from '@vue/test-utils';
+import {MountOptions, Wrapper, mount} from '@vue/test-utils';
+import {
+  BButton, BCard, BSkeleton, BTh,
+} from 'bootstrap-vue';
 import config, {localVue, cloneStore, addBootstrapToVue} from '@/../tests/init';
 
 import CampaignTableList from './campaign-table-list.vue';
-import { campaigns } from '@/../.storybook/mock/campaigns-list';
-import { BCard, BSkeleton } from 'bootstrap-vue';
+import {campaigns} from '@/../.storybook/mock/campaigns-list';
 import campaignTableListRowVue from './campaign-table-list-row.vue';
 import tableApiErrorVue from '../commons/table-api-error.vue';
 
@@ -18,19 +20,17 @@ const buildDefaultStore = (): ReturnType<typeof cloneStore> => {
   store.modules.campaigns.actions.GET_DIMENSIONS_FILTERS = vi.fn();
   store.modules.campaigns.actions.GET_CAMPAIGNS_LIST = vi.fn();
   return store;
-}
+};
 
 const buildWrapper = (
   options: MountOptions<any> = {},
   store: ReturnType<typeof cloneStore> = buildDefaultStore(),
-) => {
-  return mount(CampaignTableList, {
-    localVue,
-    store: new Vuex.Store(store),
-    ...config,
-    ...options,
-  });
-}
+) => mount(CampaignTableList, {
+  localVue,
+  store: new Vuex.Store(store),
+  ...config,
+  ...options,
+});
 
 describe('CampaignTableList', () => {
   beforeEach(() => {
@@ -42,7 +42,7 @@ describe('CampaignTableList', () => {
       const wrapper = buildWrapper({
         propsData: {
           loading: false,
-        }
+        },
       });
 
       // Overall card
@@ -64,7 +64,7 @@ describe('CampaignTableList', () => {
       const wrapper = buildWrapper({
         propsData: {
           loading: false,
-        }
+        },
       });
       await wrapper.setData({
         fetchingCampaigns: true,
@@ -93,7 +93,7 @@ describe('CampaignTableList', () => {
       const wrapper = buildWrapper({
         propsData: {
           loading: false,
-        }
+        },
       }, store);
 
       // Overall card
@@ -118,7 +118,7 @@ describe('CampaignTableList', () => {
       const wrapper = buildWrapper({
         propsData: {
           loading: true,
-        }
+        },
       }, store);
 
       // Overall card
@@ -143,7 +143,7 @@ describe('CampaignTableList', () => {
       const wrapper = buildWrapper({
         propsData: {
           loading: false,
-        }
+        },
       }, store);
 
       expect(wrapper.findComponent(BCard).exists()).toBe(false);
@@ -151,9 +151,78 @@ describe('CampaignTableList', () => {
   });
 
   describe('Header', () => {
-    it.todo('has the proper number of columns');
-    it.todo('has date + performance columns sortable');
-    it.todo('adds a background color on performance columns');
+    it('has the proper number of columns', () => {
+      const wrapper = buildWrapper({
+        propsData: {
+          loading: false,
+        },
+      });
+
+      const headerCells = wrapper.findAllComponents(BTh);
+
+      expect(headerCells).toHaveLength(11);
+      expect(headerCells.at(0).text()).toEqual('');
+      expect(headerCells.at(1).find('span').text()).toEqual('Title');
+      expect(headerCells.at(2).find('span').text()).toEqual('Date');
+      expect(headerCells.at(3).find('span').text()).toEqual('Impressions');
+      expect(headerCells.at(4).find('span').text()).toEqual('Clicks');
+      expect(headerCells.at(5).find('span').text()).toEqual('Conversions');
+      expect(headerCells.at(6).find('span').text()).toEqual('Sales');
+      expect(headerCells.at(7).find('span').text()).toEqual('Ad spend');
+      expect(headerCells.at(8).find('span').text()).toEqual('Target country');
+      expect(headerCells.at(9).find('span').text()).toEqual('Daily budget');
+      expect(headerCells.at(10).find('span').text()).toEqual('Actions');
+    });
+
+    it('has date + performance columns sortable', () => {
+      const wrapper = buildWrapper({
+        propsData: {
+          loading: false,
+        },
+      });
+
+      const isSortable = (w: Wrapper<any, Element>): boolean => w.findComponent(BButton).exists();
+
+      const headerCells = wrapper.findAllComponents(BTh);
+
+      expect(headerCells).toHaveLength(11);
+      expect(isSortable(headerCells.at(0))).toBe(false);
+      expect(isSortable(headerCells.at(1))).toBe(false);
+      expect(isSortable(headerCells.at(2))).toBe(true);
+      expect(isSortable(headerCells.at(3))).toBe(true);
+      expect(isSortable(headerCells.at(4))).toBe(true);
+      expect(isSortable(headerCells.at(5))).toBe(true);
+      expect(isSortable(headerCells.at(6))).toBe(true);
+      expect(isSortable(headerCells.at(7))).toBe(true);
+      expect(isSortable(headerCells.at(8))).toBe(false);
+      expect(isSortable(headerCells.at(9))).toBe(false);
+      expect(isSortable(headerCells.at(10))).toBe(false);
+    });
+
+    it('adds a background color on performance columns', () => {
+      const wrapper = buildWrapper({
+        propsData: {
+          loading: false,
+        },
+      });
+
+      const hasPerformanceClass = (w: Wrapper<any, Element>): boolean => w.classes().includes('ps_gs-table-performance-header');
+
+      const headerCells = wrapper.findAllComponents(BTh);
+
+      expect(headerCells).toHaveLength(11);
+      expect(hasPerformanceClass(headerCells.at(0))).toBe(false);
+      expect(hasPerformanceClass(headerCells.at(1))).toBe(false);
+      expect(hasPerformanceClass(headerCells.at(2))).toBe(false);
+      expect(hasPerformanceClass(headerCells.at(3))).toBe(true);
+      expect(hasPerformanceClass(headerCells.at(4))).toBe(true);
+      expect(hasPerformanceClass(headerCells.at(5))).toBe(true);
+      expect(hasPerformanceClass(headerCells.at(6))).toBe(true);
+      expect(hasPerformanceClass(headerCells.at(7))).toBe(true);
+      expect(hasPerformanceClass(headerCells.at(8))).toBe(false);
+      expect(hasPerformanceClass(headerCells.at(9))).toBe(false);
+      expect(hasPerformanceClass(headerCells.at(10))).toBe(false);
+    });
   });
 
   describe('Sorting', () => {
@@ -161,7 +230,7 @@ describe('CampaignTableList', () => {
     it.todo('sets as Ascending on first clic');
     it.todo('sets as Descending when current sorting is Ascending');
     it.todo('sets as Ascending when current sorting is Descending');
-    it.todo('allows only one column to be sorted')
+    it.todo('allows only one column to be sorted');
   });
 
   describe('Pagination', () => {
