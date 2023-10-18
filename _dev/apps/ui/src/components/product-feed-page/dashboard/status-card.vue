a2f525562f9d212d643c9a3e7294bccbf09fbb6b
Thomas Nabord <thomas.nabord@prestashop.com>
2023-10-18 12:08:56 +0100

    Apply color on status card border even when reversing colors

--- _dev/apps/ui/src/components/product-feed-page/dashboard/status-card.vue
+++ _dev/apps/ui/src/components/product-feed-page/dashboard/status-card.vue
@@ -121,9 +121,6 @@ export default defineComponent({
       return this.status.value ?? '--';
     },
     badgeBorderVariant(): string|null {
-      if (this.status.reverseColors) {
-        return null;
-      }
       return this.status.variant;
     },
     badgeTextVariant(): string|null {
