576396cbb0a8c57e0e5c973b4fe661452bf598b3
Thomas Nabord <thomas.nabord@prestashop.com>
2023-10-24 17:58:01 +0200

    Fis issue with EOF

--- tests/unit/Conversion/SnippetUpdaterTest.php
+++ tests/unit/Conversion/SnippetUpdaterTest.php
@@ -48,7 +48,7 @@ class SnippetUpdaterTest extends TestCase
             gtag('js', new Date());
             gtag('config', 'AW-000000000', {'allow_enhanced_conversions': true});
             </script>
-            EOF;
+EOF;
 
         $expectedOutput = <<<EOF
             <!-- Google tag (gtag.js) -->
@@ -59,7 +59,7 @@ class SnippetUpdaterTest extends TestCase
             gtag('js', new Date());
             gtag('config', 'AW-000000000');
             </script>
-            EOF;
+EOF;
 
         $this->assertSame(
             $expectedOutput,
