5d1a15dcbf8d6b8604489f884d35486310164ac1
Thomas Nabord <thomas.nabord@prestashop.com>
2023-11-24 16:11:41 +0000

    Add toast and refresher of Google Ads account

--- _dev/apps/ui/src/components/enhanced-conversions/enhanced-conversions-card.vue
+++ _dev/apps/ui/src/components/enhanced-conversions/enhanced-conversions-card.vue
@@ -62,7 +62,7 @@ export default defineComponent({
   components: {
     AlertEcReady,
     AlertSignGadsTos
-},
+  },
   data() {
     return {
       showReadyToEnableFeature: false as boolean,
--- _dev/apps/ui/stories/enhanced-conversion-card.stories.ts
+++ _dev/apps/ui/stories/enhanced-conversion-card.stories.ts
@@ -1,19 +1,41 @@
 import cloneDeep from 'lodash.clonedeep';
+import {rest} from 'msw';
 import EnhancedConversionsCard from '@/components/enhanced-conversions/enhanced-conversions-card.vue';
 import {State as CampaignsState} from '@/store/modules/campaigns/state';
 import {State as GoogleAdsState} from '@/store/modules/google-ads/state';
 import { googleAdsAccountChosen, googleAdsAccountChosenWithSignedTos } from '@/../.storybook/mock/google-ads';
-
+import NotificationPanel from '@/components/enhanced-conversions/notification-panel.vue';
 export default {
   title: 'Enhanced Conversions/Card',
   component: EnhancedConversionsCard
 };
 
+const componentMsw = {
+  handlers: [
+    rest.post('/', (req, res, ctx) => {
+      const payload = req.body.action;
+
+      if (payload === 'setEnhancedConversions') {
+        return res(
+          ctx.json({
+            success: true
+          }),
+        );
+      }
+      return null;
+    }),
+  ],
+};
+
 const Template = (args, { argTypes }) => ({
   props: Object.keys(argTypes),
-  components: { EnhancedConversionsCard },
+  components: {
+    EnhancedConversionsCard,
+    NotificationPanel,
+  },
   template: `
     <div>
+      <notification-panel />
       <enhanced-conversions-card v-bind="$props" />
     </div>
   `,
@@ -27,6 +49,9 @@ Default.args = {
     (this.$store.state.googleAds as GoogleAdsState) = cloneDeep(googleAdsAccountChosen);
   },
 }
+Default.parameters = {
+  msw: componentMsw,
+};
 
 export const WithTosSigned:any = Template.bind({});
 WithTosSigned.args = {
@@ -35,3 +60,6 @@ WithTosSigned.args = {
     (this.$store.state.googleAds as GoogleAdsState) = cloneDeep(googleAdsAccountChosenWithSignedTos);
   },
 }
+WithTosSigned.parameters = {
+  msw: componentMsw,
+};
