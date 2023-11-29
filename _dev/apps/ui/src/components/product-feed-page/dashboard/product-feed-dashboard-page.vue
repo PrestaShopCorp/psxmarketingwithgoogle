8696a7f386ce3c9ed189bbf3b9d7e516a4257377
Thomas Nabord <thomas.nabord@prestashop.com>
2023-11-29 19:12:19 +0100

    Plug forgoten EC alert on campaign & catalog pages

--- _dev/apps/ui/src/components/product-feed-page/dashboard/product-feed-dashboard-page.vue
+++ _dev/apps/ui/src/components/product-feed-page/dashboard/product-feed-dashboard-page.vue
@@ -8,9 +8,17 @@
     >
       <p> {{ $t('productFeedPage.alert.alertSuccess') }}</p>
     </PsToast>
-    <alert-sign-gads-tos
-      v-else-if="allDataLoaded && !GET_CONVERSIONS_TERMS_OF_SERVICES_SIGNED"
-    />
+    <template
+      v-else-if="allDataLoaded"
+    >
+      <alert-sign-gads-tos
+        v-if="!GET_CONVERSIONS_TERMS_OF_SERVICES_SIGNED"
+      />
+      <alert-ec-ready
+        v-else-if="!GET_ENHANCED_CONVERSIONS_STATUS"
+        :is-on-configuration-page="false"
+      />
+    </template>
     <sync-overview
       :in-need-of-configuration="inNeedOfConfiguration"
       :loading="!allDataLoaded"
@@ -24,11 +32,14 @@ import {mapGetters} from "vuex";
 import { RequestState } from '@/store/types';
 import SyncOverview from '@/components/product-feed-page/dashboard/sync-overview.vue';
 import PsToast from '@/components/commons/ps-toast.vue';
+import AlertEcReady from '@/components/enhanced-conversions/alert-ec-ready.vue';
 import AlertSignGadsTos from "@/components/enhanced-conversions/alert-sign-gads-tos.vue";
+import GettersTypesCampaigns from "@/store/modules/campaigns/getters-types";
 import GettersTypesGoogleAds from "@/store/modules/google-ads/getters-types";
 
 export default defineComponent({
   components: {
+    AlertEcReady,
     AlertSignGadsTos,
     PsToast,
     SyncOverview,
@@ -42,6 +53,9 @@ export default defineComponent({
   computed: {
     ...mapGetters("googleAds", [
       GettersTypesGoogleAds.GET_CONVERSIONS_TERMS_OF_SERVICES_SIGNED,
+    ]),
+    ...mapGetters("campaigns", [
+      GettersTypesCampaigns.GET_ENHANCED_CONVERSIONS_STATUS,
     ]),
     allDataLoaded(): boolean {
       return this.$store.state.productFeed.warmedUp === RequestState.SUCCESS;
