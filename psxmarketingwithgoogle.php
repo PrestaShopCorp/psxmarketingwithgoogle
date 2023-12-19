34a1ae2c1b7540e8bb94ff7d975aafbbb46a05f9
Thomas Nabord <thomas.nabord@prestashop.com>
2023-12-19 11:03:29 +0100

    Bump to v1.63.0

--- psxmarketingwithgoogle.php
+++ psxmarketingwithgoogle.php
@@ -74,7 +74,7 @@ class PsxMarketingWithGoogle extends Module
     {
         $this->name = 'psxmarketingwithgoogle';
         $this->tab = 'advertising_marketing';
-        $this->version = '1.61.1';
+        $this->version = '1.63.0';
         $this->author = 'PrestaShop';
         $this->need_instance = 0;
         $this->module_key = '16b273e77e02c0cc36cd006463951593';
