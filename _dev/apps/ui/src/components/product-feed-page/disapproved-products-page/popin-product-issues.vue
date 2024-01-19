3c9e1a46fb7bd2ae2fcad268143773b2a05f9538
Thomas Nabord <thomas.nabord@prestashop.com>
2024-01-19 17:26:35 +0000

    Make clean display of tooltips

--- _dev/apps/ui/src/components/product-feed-page/disapproved-products-page/popin-product-issues.vue
+++ _dev/apps/ui/src/components/product-feed-page/disapproved-products-page/popin-product-issues.vue
@@ -31,12 +31,18 @@ import {defineComponent, PropType} from 'vue';
 import PsModal from '@/components/commons/ps-modal.vue';
 import CollapsingIssues from '@/components/render-issues/collapsing-issues.vue';
 import {ProductIssue} from '@/components/render-issues/product-issues.types';
+import { BTooltip } from 'bootstrap-vue';
 
 export default defineComponent({
   name: 'PopinProductIssues',
   components: {
     PsModal,
     CollapsingIssues,
+  },
+  data() {
+    return {
+      tooltipsComponents: [] as BTooltip[],
+    };
   },
   props: {
     issues: {
@@ -52,6 +58,37 @@ export default defineComponent({
     getProductBaseUrl() {
       return this.$store.getters['app/GET_PRODUCT_DETAIL_BASE_URL'];
     },
+  },
+  methods: {
+    findAndCreateTooltips(): void {
+      // Initialisation of Tooltips
+      const tooltips = this.$el.querySelectorAll('.tooltip');
+      
+      tooltips.forEach((tooltip) => {
+        const tooltipText = tooltip.querySelector('.tooltip-text');
+        const tooltipIcon = tooltip.querySelector('.tooltip-icon');
+
+        const $tooltip = (this.tooltipsComponents = new BTooltip({
+          parent: this,
+
+          propsData: {
+            target: tooltipIcon,
+            container: '#psxMktgWithGoogleApp',
+            title: tooltipText?.textContent,
+          }
+        }));
+        
+        $tooltip.$mount();
+      });
+    },
+    unloadTooltips(): void {
+      this.tooltipsComponents.forEach((tooltip) => tooltip.$destroy());
+    },
+  },
+  mounted() {
+    // Because the HTML code comes from Google API, we have to bend it
+    // to use specific features of our application.
+    this.findAndCreateTooltips();
   },
 });
 </script>
