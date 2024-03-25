afb786199b4bf9c796aab5a6f580d79f16ac4798
Thomas Nabord <thomas.nabord@prestashop.com>
2024-03-25 18:04:51 +0000

    Introduce routes to get product filter options

---
+++ classes/ProductFilter/Options/AttributeOptionsProvider.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Copyright since 2007 PrestaShop SA and Contributors
+ * PrestaShop is an International Registered Trademark & Property of PrestaShop SA
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Academic Free License version 3.0
+ * that is bundled with this package in the file LICENSE.md.
+ * It is also available through the world-wide-web at this URL:
+ * https://opensource.org/licenses/AFL-3.0
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@prestashop.com so we can send you a copy immediately.
+ *
+ * @author    PrestaShop SA and Contributors <contact@prestashop.com>
+ * @copyright Since 2007 PrestaShop SA and Contributors
+ * @license   https://opensource.org/licenses/AFL-3.0 Academic Free License version 3.0
+ */
+
+namespace PrestaShop\Module\PsxMarketingWithGoogle\ProductFilter\Options;
+
+use PrestaShop\Module\PsxMarketingWithGoogle\Repository\AttributesRepository;
+
+class AttributeOptionsProvider implements OptionsProviderInterface
+{
+    /**
+     * @var AttributesRepository
+     */
+    protected $attributeRepository;
+
+    public function __construct(
+        AttributesRepository $attributeRepository
+    ) {
+        $this->attributeRepository = $attributeRepository;
+    }
+
+    public function getOptions(): array
+    {
+        return $this->attributeRepository->getAllAttributes();
+    }
+}
