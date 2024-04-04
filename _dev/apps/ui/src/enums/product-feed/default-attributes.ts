12e42a29f2685bc720fcb88cde286b06aec0e5e1
ClaraLpresta <clara.laporte@prestashop.com>
2024-04-04 11:14:37 +0200

    [Product Filter ] attribute field

---
+++ _dev/apps/ui/src/enums/product-feed/default-attributes.ts
@@ -0,0 +1,9 @@
+enum DefaultAttributes {
+  PRICE = 'price',
+  BRAND = 'brand',
+  CATEGORY = 'category',
+  PRODUCT_ID = 'productId',
+  OUT_OF_STOCK = 'outOfStock',
+}
+
+export default DefaultAttributes;
