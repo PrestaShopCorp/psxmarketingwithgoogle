32305b470d55e6e48f4bc087946e24716352f3e8
ClaraLpresta <clara.laporte@prestashop.com>
2024-04-09 17:52:42 +0200

    create a line for a filter

--- _dev/apps/ui/src/components/commons/input-text-with-tag.vue
+++ _dev/apps/ui/src/components/commons/input-text-with-tag.vue
@@ -1,12 +1,33 @@
+<!-- eslint-disable max-len -->
 <template>
   <div class="input-text-with-tag">
-    <div class="container" :class="{ 'has-error': hasError }">
-      <span v-for="(tag, index) in tags" :key="index" class="tag" :style="`flex-shrink: ${index}`">
-        {{ tag }}<button @click="deleteTag(tag)" class="material-icons">close</button>
+    <div
+      class="container"
+      :class="{ 'has-error': hasError }"
+    >
+      <span
+        v-for="(tag, index) in tags"
+        :key="index"
+        class="tag"
+        :style="`flex-shrink: ${index}`"
+      >
+        {{ tag }}<button
+          @click.stop.prevent="deleteTag(tag)"
+          class="material-icons"
+        >close</button>
       </span>
-      <input type="text" v-model="newTag" @keyup.enter="addTag" />
+      <input
+        type="text"
+        v-model="newTag"
+        :placeholder="setPlaceholder"
+        :disabled="disabled"
+        @keydown.enter.stop.prevent="addTag"
+      >
     </div>
-    <b-form-invalid-feedback v-if="hasError" :state="false">
+    <b-form-invalid-feedback
+      v-if="hasError"
+      :state="false"
+    >
       {{ $t('general.errorsMessages.enterValue') }}
     </b-form-invalid-feedback>
   </div>
@@ -20,12 +41,17 @@ export default defineComponent({
   data() {
     return {
       newTag: '',
-      tags: []
-    }
+      tags: [],
+    };
   },
   props: {
     hasError: {
       type: Boolean || undefined,
+      required: false,
+      default: false,
+    },
+    disabled: {
+      type: Boolean,
       required: false,
       default: false,
     },
@@ -38,8 +64,12 @@ export default defineComponent({
     },
     deleteTag(tag) {
       this.tags.splice(this.tags.indexOf(tag), 1);
-    }
+    },
   },
-  computed: {},
+  computed: {
+    setPlaceholder() {
+      return this.$i18n.t('productFeedSettings.productSelection.lineFilter.value.enterValue') as string;
+    },
+  },
 });
 </script>
