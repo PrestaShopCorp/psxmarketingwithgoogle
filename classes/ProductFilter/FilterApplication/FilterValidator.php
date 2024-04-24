c53527c99b454332d6fc9ce9e578d55a7c8a0d96
Alexis Guyomar <alexis.guyomar@prestashop.com>
2024-04-24 16:52:27 +0200

    feat: add does not contain condition and map attribute with condition output

--- classes/ProductFilter/FilterApplication/FilterValidator.php
+++ classes/ProductFilter/FilterApplication/FilterValidator.php
@@ -21,83 +21,119 @@
 namespace PrestaShop\Module\PsxMarketingWithGoogle\ProductFilter\FilterApplication;
 
 use InvalidArgumentException;
-use PrestaShop\Module\PsxMarketingWithGoogle\ProductFilter\AttributeType;
-use PrestaShop\Module\PsxMarketingWithGoogle\ProductFilter\Condition;
+use PrestaShop\Module\PsxMarketingWithGoogle\ProductFilter\AttributeMapConditionOutput;
 
 class FilterValidator
 {
     /**
      * @throws InvalidArgumentException
      *
-     * TODO: Input input checking with types if possible.
      * See https://phpstan.org/writing-php-code/phpdoc-types#general-arrays
      */
     public function validate(array $filters): void
     {
         foreach ($filters as $index => $filter) {
-            $this->hasValueCompliantWithCondition($filter, $index);
-
-            switch ($filter['attribute']) {
-                case AttributeType::BRAND:
-                    break;
-                case AttributeType::CATEGORY:
-                    $this->mustContainNumbers($filter, $index);
-                    $this->mustContainPositiveNumbers($filter, $index);
-                    break;
-                case AttributeType::CUSTOM_ATTRIBUTE:
-                    $this->mustHaveSeveralValues($filter, $index);
-                    $this->mustContainLocalizedValues($filter, $index);
-                    break;
-                case AttributeType::PRICE:
-                    $this->mustContainNumbers($filter, $index);
-                    break;
-                case AttributeType::PRODUCT_ID:
-                    $this->mustContainNumbers($filter, $index);
-                    $this->mustContainPositiveNumbers($filter, $index);
-                    break;
-                case AttributeType::OUT_OF_STOCK:
-                    $this->mustHaveOneValue($filter, $index);
-                    $this->mustBeBoolean($filter, $index);
-                    break;
+            if (!isset(AttributeMapConditionOutput::MAP[$filter['attribute']])) {
+                throw new InvalidArgumentException('Filter #' . $index . ' has no valid "attribute" field.');
             }
+
+            $attributeConditions = AttributeMapConditionOutput::MAP[$filter['attribute']];
+
+            if (!isset($attributeConditions[$filter['condition']])) {
+                throw new InvalidArgumentException('Filter #' . $index . ' has no valid "condition" field.');
+            }
+
+            $conditionRequirements = $attributeConditions[$filter['condition']];
+
+            $this->hasValueCompliantWithCondition($filter, $index, $conditionRequirements);
         }
     }
 
-    protected function hasValueCompliantWithCondition($filter, int $index): void
+    /**
+     * @throws InvalidArgumentException
+     *
+     * @param $filter
+     * @param int $index
+     * @param $conditionRequirements
+     *
+     * @return void
+     */
+    protected function hasValueCompliantWithCondition($filter, int $index, $conditionRequirements): void
     {
-        switch ($filter['condition']) {
-            case Condition::CONTAINS:
-                $this->mustHaveOneValue($filter, $index);
+        // number of values check
+        if ($conditionRequirements['multiple']) {
+            $this->mustHaveSeveralValues($filter, $index);
+        } else {
+            $this->mustHaveOneValue($filter, $index);
+        }
+
+        // type check
+        switch ($conditionRequirements['type']) {
+            case AttributeMapConditionOutput::STRING:
+                $this->mustBeString($filter, $index);
                 break;
-            case Condition::GREATER:
-                $this->mustHaveOneValue($filter, $index);
+            case AttributeMapConditionOutput::INT:
+                $this->mustBeNumber($filter, $index);
+                if ($conditionRequirements['positive']) {
+                    $this->mustBePositiveNumber($filter, $index);
+                }
                 break;
-            case Condition::LOWER:
-                $this->mustHaveOneValue($filter, $index);
+            case AttributeMapConditionOutput::BOOLEAN:
+                $this->mustBeBoolean($filter, $index);
                 break;
-            case Condition::IS:
-                // Either single or multiple values
-                break;
-            case Condition::IS_NOT:
-                $this->mustHaveSeveralValues($filter, $index);
+            case AttributeMapConditionOutput::OBJECT:
+                $this->mustBeValidObject($filter, $index, $conditionRequirements['keys']);
                 break;
         }
     }
 
+    /**
+     * @throws InvalidArgumentException
+     *
+     * @param $filter
+     * @param int $index
+     *
+     * @return void
+     */
     protected function mustHaveOneValue($filter, int $index): void
     {
         if (isset($filter['values'])) {
             throw new InvalidArgumentException('Filter #' . $index . ' is malformed, a field "value" is excepted instead of "values".');
         }
+        if (!isset($filter['value'])) {
+            throw new InvalidArgumentException('Filter #' . $index . ' requires a "value" field.');
+        }
     }
 
+    /**
+     * @throws InvalidArgumentException
+     *
+     * @param $filter
+     * @param int $index
+     *
+     * @return void
+     */
     protected function mustHaveSeveralValues($filter, int $index): void
     {
         if (isset($filter['value'])) {
             throw new InvalidArgumentException('Filter #' . $index . ' is malformed, a field "values" is excepted instead of "value".');
         }
+        if (!isset($filter['values'])) {
+            throw new InvalidArgumentException('Filter #' . $index . ' requires a "values" field.');
+        }
+        if (!is_array($filter['values']) || $this->isAssociativeArray($filter['values'])) {
+            throw new InvalidArgumentException('Filter #' . $index . ' is malformed, "values" field need to be an array of value.');
+        }
     }
 
+    /**
+     * @throws InvalidArgumentException
+     *
+     * @param $filter
+     * @param int $index
+     *
+     * @return void
+     */
     protected function mustBeBoolean($filter, int $index): void
     {
         if (gettype($filter['value']) !== 'boolean') {
@@ -105,9 +141,46 @@ class FilterValidator
         }
     }
 
-    protected function mustContainNumbers($filter, int $index): void
+    /**
+     * @throws InvalidArgumentException
+     *
+     * @param $filter
+     * @param int $index
+     *
+     * @return void
+     */
+    protected function mustBeString($filter, int $index): void
     {
-        $checkValueIsNumeric = function ($value) use ($index): void {
+        $checkValueIsString = function ($value) use ($index): void {
+            if (!is_string($value)) {
+                throw new InvalidArgumentException('Value ' . $value . " of filter #$index must be a string.");
+            }
+        };
+
+        // Single Value
+        if (isset($filter['value'])) {
+            $checkValueIsString($filter['value']);
+        }
+
+        // Multiple values
+        if (isset($filter['values'])) {
+            foreach ($filter['values'] as $value) {
+                $checkValueIsString($value);
+            }
+        }
+    }
+
+    /**
+     * @throws InvalidArgumentException
+     *
+     * @param $filter
+     * @param int $index
+     *
+     * @return void
+     */
+    protected function mustBeNumber($filter, int $index): void
+    {
+        $checkValueIsNumber = function ($value) use ($index): void {
             if (!is_numeric($value)) {
                 throw new InvalidArgumentException('Value ' . $value . " of filter #$index must be a number.");
             }
@@ -115,18 +188,26 @@ class FilterValidator
 
         // Single Value
         if (isset($filter['value'])) {
-            $checkValueIsNumeric($filter['value']);
+            $checkValueIsNumber($filter['value']);
         }
 
         // Multiple values
         if (isset($filter['values'])) {
             foreach ($filter['values'] as $value) {
-                $checkValueIsNumeric($value);
+                $checkValueIsNumber($value);
             }
         }
     }
 
-    protected function mustContainPositiveNumbers($filter, int $index): void
+    /**
+     * @throws InvalidArgumentException
+     *
+     * @param $filter
+     * @param int $index
+     *
+     * @return void
+     */
+    protected function mustBePositiveNumber($filter, int $index): void
     {
         $checkValueIsPositive = function ($value) use ($index): void {
             if ((int) $value <= 0) {
@@ -147,20 +228,43 @@ class FilterValidator
         }
     }
 
-    protected function mustContainLocalizedValues($filter, int $index): void
+    protected function mustBeValidObject($filter, int $index, array $keys): void
     {
-        foreach ($filter['values'] as $localizedValueIndex => $localizedValue) {
-            if (empty($localizedValue['value'])) {
-                throw new InvalidArgumentException('Missing field "value" in filter #' . $index . ', key #' . $localizedValueIndex . '.');
+        $checkValueIsValidObject = function ($value) use ($index, $keys): void {
+            if (!$this->isAssociativeArray($value)) {
+                throw new InvalidArgumentException('Value ' . json_encode($value) . " of filter #$index is not an object.");
             }
 
-            if (empty($localizedValue['language'])) {
-                throw new InvalidArgumentException('Missing field "language" in filter #' . $index . ', key #' . $localizedValueIndex . '.');
+            foreach ($keys as $key) {
+                if (!array_key_exists($key, $value)) {
+                    throw new InvalidArgumentException('Value ' . json_encode($value) . " of filter #$index does not have the required '" . $key . '\' key.');
+                }
             }
+        };
 
-            if (empty($localizedValue['key'])) {
-                throw new InvalidArgumentException('Missing field "key" in filter #' . $index . ', key #' . $localizedValueIndex . '.');
+        // Single Value
+        if (isset($filter['value'])) {
+            $checkValueIsValidObject($filter['value']);
+        }
+
+        // Multiple values
+        if (isset($filter['values'])) {
+            foreach ($filter['values'] as $value) {
+                $checkValueIsValidObject($value);
             }
         }
+    }
+
+    protected function isAssociativeArray($array) {
+        if (!is_array($array)) {
+            return false;
+        }
+
+        foreach (array_keys($array) as $key) {
+            if (!is_string($key)) {
+                return false;
+            }
+        }
+        return true;
     }
 }
