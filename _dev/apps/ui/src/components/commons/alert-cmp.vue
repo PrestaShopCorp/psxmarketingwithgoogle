877e1c4d79877d9bcf3fb1ebfb8faf0a5e87041f
Alexis Guyomar <alexis.guyomar@prestashop.com>
2024-05-17 17:04:41 +0200

    feat: add cmp alert

---
+++ _dev/apps/ui/src/components/commons/alert-cmp.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="col-12">
+    <b-alert
+      v-if="display"
+      variant="info"
+      class=""
+      show
+    >
+      <div>
+        <div class="h3">
+          {{ $t('onboarding.cmpAlert.title') }}
+        </div>
+        <p class="mb-2">
+          {{ $t('onboarding.cmpAlert.paragraph') }}
+        </p>
+        <ol>
+          <li>{{ $t('onboarding.cmpAlert.list1') }}</li>
+          <i18n
+            path="onboarding.cmpAlert.list2"
+            tag="li"
+          >
+            <template #link>
+              <a
+                :href="GDPRModuleLink"
+                target="_blank"
+                rel="noopener"
+              >{{ $t('onboarding.cmpAlert.moduleGDPR') }}</a>
+            </template>
+          </i18n>
+        </ol>
+        <b-button
+          size="sm"
+          variant="outline-primary"
+          @click="onClick"
+        >
+          I understand
+        </b-button>
+      </div>
+    </b-alert>
+  </div>
+</template>
+
+<script>
+import SegmentGenericParams from '@/utils/SegmentGenericParams';
+
+export default {
+  name: 'AlertCMP',
+  data() {
+    return {
+      display: true,
+    };
+  },
+  methods: {
+    sendEvent() {
+      this.$segment.track('[GGL] Understand CMP requirement', {
+        module: 'psxmarketingwithgoogle',
+        params: SegmentGenericParams,
+      });
+    },
+    onClick() {
+      localStorage.setItem('alert-cmp-closed', 1);
+      this.sendEvent();
+      this.display = false;
+    },
+  },
+  computed: {
+    GDPRModuleLink() {
+      return `https://addons.prestashop.com/${this.$i18n.locale}/legal/21644-cookies-gdpr-law-blocker-google-consent-mode-v2.html`;
+    },
+  },
+};
+</script>
