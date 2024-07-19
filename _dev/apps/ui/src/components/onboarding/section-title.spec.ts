d40ead48f5adf18dbd30e34bd455cd44a206c12e
ClaraLpresta <clara.laporte@prestashop.com>
2024-07-19 11:55:43 +0200

    update test of section title

--- _dev/apps/ui/src/components/onboarding/section-title.spec.ts
+++ _dev/apps/ui/src/components/onboarding/section-title.spec.ts
@@ -1,8 +1,6 @@
-import Vuex from 'vuex';
-
 import cloneDeep from 'lodash.clonedeep';
 import {MountOptions, shallowMount} from '@vue/test-utils';
-import config, {localVue, cloneStore} from '@/../tests/init';
+import {localVue, cloneStore} from '@/../tests/init';
 
 import {initialStateApp} from '../../../.storybook/mock/state-app';
 import SectionTitle from './section-title.vue';
@@ -16,103 +14,27 @@ const buildWrapper = (
 
   return shallowMount(SectionTitle, {
     localVue,
-    store: new Vuex.Store(store),
-    ...config,
-    stubs: {
-      VueShowdown: true,
-    },
     ...options,
   });
 };
 describe('Testing differents case for the sectionTitle components', () => {
-  it('should display correctly the stepper when merchant start configuration', () => {
-    const store = cloneStore();
+  it('should display correctly the stepper title', () => {
     const wrapper = buildWrapper({
       propsData: {
-        stepTitle: 'step 1',
-        stepNumber: 1,
+        stepTitle: 'Step Title',
         isEnabled: true,
-        isDone: false,
       },
-      store: new Vuex.Store(store),
     });
-
-    expect(wrapper.find('.ps_gs-section-title .circle').text()).toBe('1');
-    expect(wrapper.find('.ps_gs-section-title .circle').classes()).toContain('bg-circle-active');
-    expect(wrapper.find('.circle').classes()).not.toContain('bg-circle-disabled');
-    expect(wrapper.find('.material-icons').exists()).toBeFalsy();
+    expect(wrapper.find('.ps_gs-section-title').text()).toBe('Step Title');
   });
-
-  it('should display a checkmark when merchant finished the first step', () => {
-    const store = cloneStore();
+  it('should have disabled title', () => {
     const wrapper = buildWrapper({
       propsData: {
-        stepTitle: 'step 1',
-        stepNumber: 1,
-        isEnabled: true,
-        isDone: true,
-      },
-      store: new Vuex.Store(store),
-    });
-
-    expect(wrapper.find('.ps_gs-section-title .circle').exists()).toBeFalsy();
-    expect(wrapper.find('.material-icons').exists()).toBeTruthy();
-  });
-
-  it('should display a circle disable', () => {
-    const store = cloneStore();
-    const wrapper = buildWrapper({
-      propsData: {
-        stepTitle: 'step 2',
-        stepNumber: 2,
+        stepTitle: 'Step Title',
         isEnabled: false,
-        isDone: false,
       },
-      store: new Vuex.Store(store),
     });
 
-    expect(wrapper.find('.ps_gs-section-title .circle').classes()).toContain('bg-circle-disabled');
-    expect(wrapper.find('.material-icons').exists()).toBeFalsy();
-  });
-
-  it('should display checkmark, current, disable steps', () => {
-    const store = cloneStore();
-    const steps = [
-      {
-        stepTitle: 'step 1',
-        stepNumber: 1,
-        isEnabled: true,
-        isDone: true,
-      },
-      {
-        stepTitle: 'step 2',
-        stepNumber: 2,
-        isEnabled: true,
-        isDone: false,
-      },
-      {
-        stepTitle: 'step 3',
-        stepNumber: 3,
-        isEnabled: false,
-        isDone: false,
-      },
-    ];
-    const wrapper: any = [];
-
-    for (let i = 0; i < steps.length; i += 1) {
-      wrapper.push(buildWrapper({
-        propsData: steps[i],
-        store: new Vuex.Store(store),
-      }));
-    }
-
-    expect(wrapper[0].find('.ps_gs-section-title .circle').exists()).toBeFalsy();
-    expect(wrapper[0].find('.material-icons').exists()).toBeTruthy();
-
-    expect(wrapper[1].find('.material-icons').exists()).toBeFalsy();
-    expect(wrapper[1].find('.circle').classes()).not.toContain('bg-circle-disabled');
-
-    expect(wrapper[2].find('.ps_gs-section-title .circle').classes()).toContain('bg-circle-disabled');
-    expect(wrapper[2].find('.material-icons').exists()).toBeFalsy();
+    expect(wrapper.find('.ps_gs-section-title').classes()).toContain('ps_gs-section-title--disabled');
   });
 });
