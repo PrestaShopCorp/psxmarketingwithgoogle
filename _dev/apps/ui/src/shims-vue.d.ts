933e37f75fb824d371a0abc0c86f1cfdd3ec0223
ClaraLpresta <clara.laporte@prestashop.com>
2024-08-16 15:32:51 +0200

    Make type of segment method on project root

--- _dev/apps/ui/src/shims-vue.d.ts
+++ _dev/apps/ui/src/shims-vue.d.ts
@@ -13,6 +13,14 @@ declare module 'vue/types/vue' {
   }
 }
 
+declare module 'vue' {
+  interface ComponentCustomProperties {
+    $root: Vue & {
+      identifySegment: () => void;
+    }
+  }
+}
+
 declare module '*.svg' {
   import Vue, {VueConstructor} from 'vue';
 
