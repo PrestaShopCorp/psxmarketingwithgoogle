4120f79f77e79597b3d1fb255f25641f71f47cc4
ClaraLpresta <clara.laporte@prestashop.com>
2024-08-22 16:45:30 +0200

    fix alignment elements

--- _dev/apps/ui/src/components/monetization/monetization-messages.vue
+++ _dev/apps/ui/src/components/monetization/monetization-messages.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="container">
+  <div :class="page === 'configuration' ? 'container' : ''">
     <AlertModuleUpdate
       v-if="moduleNeedUpgrade && modaleIsClosed"
       module-name="psxmarketingwithgoogle"
