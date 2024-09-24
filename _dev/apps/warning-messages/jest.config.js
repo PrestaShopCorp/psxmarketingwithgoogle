b9205c41e4c416898c87f3e104b36c359f54695c
ClaraLpresta <clara.laporte@prestashop.com>
2024-09-24 11:16:25 +0200

    extension file added for jest

--- _dev/apps/warning-messages/jest.config.js
+++ _dev/apps/warning-messages/jest.config.js
@@ -3,10 +3,4 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'json', 'jsx'],
-  // transform: {
-  //   '^.+\\.ts?$': 'ts-jest',
-  // },
-  // moduleNameMapper: {
-  //   '^vue-i18n$': './node_modules/vue-i18n/dist/vue-i18n.cjs.js',
-  // },
 };
