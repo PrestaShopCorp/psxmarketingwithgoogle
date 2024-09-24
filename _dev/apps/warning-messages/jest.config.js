3ec76009f93fe815bd365ea0ecc0b50b5f122745
ClaraLpresta <clara.laporte@prestashop.com>
2024-09-24 10:55:49 +0200

    Fixing an import error in a .ts file (test)

--- _dev/apps/warning-messages/jest.config.js
+++ _dev/apps/warning-messages/jest.config.js
@@ -2,4 +2,11 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'json', 'jsx'],
+  transform: {
+    '^.+\\.ts?$': 'ts-jest',
+  },
+  moduleNameMapper: {
+    '^vue-i18n$': './node_modules/vue-i18n/dist/vue-i18n.cjs.js',
+  },
 };
