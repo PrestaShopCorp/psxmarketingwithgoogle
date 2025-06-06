d038039bb1130755268d166a7ada6021442c9b55
Guyomar Alexis <52718717+ga-devfront@users.noreply.github.com>
2025-06-06 10:57:05 +0200

    [FIX] icon on prestashop v9 we don't need the css anymore (#1873)
    * fix: icon on prestashop v9 we don't need the css anymore
    
    * fix: ps version const

--- psxmarketingwithgoogle.php
+++ psxmarketingwithgoogle.php
@@ -233,7 +233,9 @@ class PsxMarketingWithGoogle extends Module
             }
         }
 
-        $this->context->controller->addCSS($this->getPathUri() . 'views/css/admin/menu.css');
+        if (version_compare(_PS_VERSION_, '9.0.0', '<')) {
+            $this->context->controller->addCSS($this->getPathUri() . 'views/css/admin/menu.css');
+        }
     }
 
     public function hookDisplayHeader()
