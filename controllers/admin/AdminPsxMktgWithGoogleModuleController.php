cc2b6192ba455ab667463d08baf95b3fc3a5cff0
Alexis Guyomar <alexis.guyomar@prestashop.com>
2025-10-03 17:28:44 +0200

    fix: php stan and ci

--- controllers/admin/AdminPsxMktgWithGoogleModuleController.php
+++ controllers/admin/AdminPsxMktgWithGoogleModuleController.php
@@ -308,9 +308,9 @@ class AdminPsxMktgWithGoogleModuleController extends ModuleAdminController
         ]);
 
         if ($moduleManager->isInstalled('ps_accounts')) {
-            /** @var \PsAccounts $accountsModule */
             $accountsModule = \Module::getInstanceByName('ps_accounts');
             if ($accountsModule && version_compare($accountsModule->version, '7', '>=')) {
+                /* @phpstan-ignore-next-line */
                 $accountsCdn = $accountsModule->getParameter('ps_accounts.accounts_cdn_url');
             }
         }
